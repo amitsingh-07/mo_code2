72d54d6658b4fa71d79c18d4081c7f899d18a7bd
Ajay Maharana <ajayakumarm@ntuclink.com.sg>
2018-07-23 19:17:31 +0530

    Income / Expense UI Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { LoggerService } from './shared/logger/logger.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bfa-frontend';
+  title = 'Money Owl';
 
   constructor(private log: LoggerService) {
   }
