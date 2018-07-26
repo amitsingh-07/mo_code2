4475016f9a9c1caff1991577b39b49b6ebef2711
Teng Wei Hao <weihao_teng@providend.com>
2018-07-26 14:24:29 +0800

    BFA-10 & BFA-17 ngx-translation support added

--- src/app/guide-me/insure-assessment/insure-assessment.component.ts
+++ src/app/guide-me/insure-assessment/insure-assessment.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+
+import { IPageComponent } from '../../shared/interfaces/page-component.interface';
+import { GuideMeService } from './../guide-me.service';
+import { HeaderService } from './../../shared/header/header.service';
+
 
 @Component({
   selector: 'app-insure-assessment',
   templateUrl: './insure-assessment.component.html',
-  styleUrls: ['./insure-assessment.component.scss']
+  styleUrls: ['./insure-assessment.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class InsureAssessmentComponent implements OnInit {
+export class InsureAssessmentComponent implements IPageComponent, OnInit {
+  pageTitle: string;
 
-  constructor() { }
+  constructor(private guideMeService: GuideMeService, private router: Router,
+              public headerService: HeaderService,
+              public readonly translate: TranslateService) {
+                this.pageTitle = this.translate.instant('INSURE_ASSESSMENT.TITLE');
+              }
 
   ngOnInit() {
+    this.translate.use('en');
+    this.setPageTitle(this.pageTitle);
   }
+
+  setPageTitle(title: string) {
+    this.headerService.setPageTitle(title);
+  }
+
   goNext() {
     console.log('Next Page');
   }
