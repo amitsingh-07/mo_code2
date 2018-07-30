7b0d307c475e1bcfde76421a77153a50cf85f319
Teng Wei Hao <weihao_teng@providend.com>
2018-07-30 17:54:59 +0800

    BFA - 19 - BFA169 Updated UI implementation for new global styling - BFA171 Updated Critical Illness Component to communicate formdata with Services - BFA170 Added Preliminary Test Cases

--- src/app/guide-me/ci-assessment/ci-assessment.component.spec.ts
+++ src/app/guide-me/ci-assessment/ci-assessment.component.spec.ts
@@ -22,4 +22,37 @@ describe('CiAssessmentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should render CIAssessment title in a h2', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Critical Illness');
+  }));
+  it('should render CIAssessment description in a h6', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    // tslint:disable-next-line:max-line-length
+    expect(compiled.querySelector('h6').textContent).toContain('This coverage replaces your income during recovery period (about 2-5 years) while you are unable to work. A person usually requires Critical Illness coverage till their intended retirement age.');
+  }));
+  it('testing the dropdown', async(() => {
+    spyOn(component, 'selectRetirementAge');
+    const dropdownButton = fixture.debugElement.nativeElement.querySelector('#RetirementAgeDropDown button');
+    const dropdownItem = fixture.debugElement.nativeElement.querySelector('.dropdown-item');
+    dropdownButton.click();
+    fixture.whenStable().then(() => {
+      dropdownItem.click();
+      fixture.whenStable().then(() => {
+        expect(component.selectRetirementAge).toHaveBeenCalled();
+      });
+    });
+  }));
+  it('testing the proceed button', async(() => {
+    spyOn(component, 'goToNext');
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(component.goToNext).toHaveBeenCalled();
+    });
+  }));
 });
