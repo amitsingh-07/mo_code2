6a4148809a7a1cf65a6c8b746b9d0b867aff67af
MONEYOWL/weihao <weihao@moneyowl.com.sg>
2018-08-14 17:30:49 +0800

    BFA-24 - UI Design and Implementation

--- src/app/guide-me/guide-me.module.ts
+++ src/app/guide-me/guide-me.module.ts
@@ -5,16 +5,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { NouisliderModule } from 'ng2-nouislider';
 
-import { SharedModule } from '../shared/shared.module';
 import { CurrencyInputDirective } from '../shared/directives/currency-input.directive';
-import { GuideMeRoutingModule } from './guide-me-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { CiAssessmentComponent } from './ci-assessment/ci-assessment.component';
 import { ExpensesComponent } from './expenses/expenses.component';
 import { FinAssessmentComponent } from './fin-assessment/fin-assessment.component';
 import { GetStartedFormComponent } from './get-started/get-started-form/get-started-form.component';
 import { GetStartedComponent } from './get-started/get-started.component';
+import { GuideMeRoutingModule } from './guide-me-routing.module';
 import { HospitalPlanComponent } from './hospital-plan/hospital-plan.component';
 import { IncomeComponent } from './income/income.component';
+import { InsuranceResultsComponent } from './insurance-results/insurance-results.component';
 import { InsureAssessmentComponent } from './insure-assessment/insure-assessment.component';
 import { LiabilitiesComponent } from './liabilities/liabilities.component';
 import { LifeProtectionFormComponent } from './life-protection/life-protection-form/life-protection-form.component';
@@ -24,7 +25,6 @@ import { MyAssetsComponent } from './my-assets/my-assets.component';
 import { OcpDisabilityComponent } from './ocp-disability/ocp-disability.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProtectionNeedsComponent } from './protection-needs/protection-needs.component';
-import { InsuranceResultsComponent } from './insurance-results/insurance-results.component';
 
 @NgModule({
   imports: [
--- src/app/guide-me/insurance-results/insurance-results.component.ts
+++ src/app/guide-me/insurance-results/insurance-results.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { IPageComponent } from '../../shared/interfaces/page-component.interface';
 
+import { Router } from '../../../../node_modules/@angular/router';
+import { HeaderService } from '../../shared/header/header.service';
+import { HelpModalComponent } from '../help-modal/help-modal.component';
+import { GuideMeService } from './../guide-me.service';
+
+const assetImgPath = './assets/images/';
 @Component({
   selector: 'app-insurance-results',
   templateUrl: './insurance-results.component.html',
-  styleUrls: ['./insurance-results.component.scss']
+  styleUrls: ['./insurance-results.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class InsuranceResultsComponent implements OnInit {
-
-  constructor() { }
+export class InsuranceResultsComponent implements OnInit, IPageComponent {
+  pageTitle: string;
+  constructor(private router: Router, public headerService: HeaderService,
+              private translate: TranslateService, private guideMeService: GuideMeService,
+              public modal: NgbModal) {
+    this.translate.use('en');
+    this.translate.get('COMMON').subscribe((result: string) => {
+      this.pageTitle = this.translate.instant('INSURANCE_RESULTS.TITLE');
+      this.setPageTitle(this.pageTitle, null, false);
+    });
+  }
 
   ngOnInit() {
   }
 
+  setPageTitle(title: string, subTitle?: string, helpIcon?: boolean) {
+    this.headerService.setPageTitle(title, null, helpIcon);
+  }
 }
