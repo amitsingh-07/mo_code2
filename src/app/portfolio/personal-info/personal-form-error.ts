2f61fa83b2db6b820ed8c5db289baf23e8c84e54
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-08-27 13:46:56 +0530

    Email signature images added

--- src/app/portfolio/personal-info/personal-form-error.ts
+++ src/app/portfolio/personal-info/personal-form-error.ts
@@ -1,21 +1,23 @@
 export class PersonalFormError {
-    formFieldErrors: object = {
-        myProfile: {
-            required : 
-            {errorTitle: 'Invalid Profile', errorMessage: 'One value should be selected'}
-        },
-        gender: {
-                required : {errorTitle: 'Invalid Gender', errorMessage: 'Select the gender'}
-        },
-        dateOfBirth: {
-                required : {errorTitle: 'Invalid Birth Year',
-                        errorMessage: 'Please re-check your entry. You have keyed in an invalid birth year.'}
-        },
-        smoker: {
-                required : {errorTitle: 'Invalid Smoker', errorMessage: 'Select one option'}
-        },
-        dependent: {
-                required : {errorTitle: 'Invalid Dependent', errorMessage: 'Dependent required'}
-        }
-    };
+        formFieldErrors: object = {
+                myProfile: {
+                        required:
+                                { errorTitle: 'Invalid Profile', errorMessage: 'One value should be selected' }
+                },
+                gender: {
+                        required: { errorTitle: 'Invalid Gender', errorMessage: 'Select the gender' }
+                },
+                dateOfBirth: {
+                        required: {
+                                errorTitle: 'Invalid Birth Year',
+                                errorMessage: 'Please re-check your entry. You have keyed in an invalid birth year.'
+                        }
+                },
+                smoker: {
+                        required: { errorTitle: 'Invalid Smoker', errorMessage: 'Select one option' }
+                },
+                dependent: {
+                        required: { errorTitle: 'Invalid Dependent', errorMessage: 'Dependent required' }
+                }
+        };
 }
