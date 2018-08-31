7cc5c84db478ae378162b4227e34b1021e06cbfc
satheesan <satheesan.o@hcl.com>
2018-08-31 15:16:16 +0530

    i add portfolio recommendation splash screen for 5secs.

--- src/app/portfolio/risk-profile/risk-profile.component.ts
+++ src/app/portfolio/risk-profile/risk-profile.component.ts
@@ -3,6 +3,9 @@ import { PORTFOLIO_ROUTE_PATHS } from '../portfolio-routes.constants';
 import { HeaderService } from '../../shared/header/header.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
+import { PORTFOLIO_ROUTES } from '../portfolio-routes.constants';
+import { PortfolioService } from './../portfolio.service';
+
 @Component({
   selector: 'app-risk-profile',
   templateUrl: './risk-profile.component.html',
@@ -11,7 +14,8 @@ import { Router } from '@angular/router';
 export class RiskProfileComponent implements OnInit {
 
   constructor(public readonly translate: TranslateService, private router: Router,
-    public headerService: HeaderService) {
+    public headerService: HeaderService,
+    private portfolioService: PortfolioService) {
     this.translate.use('en');
     this.translate.get('COMMON').subscribe((result: string) => {
       
@@ -19,6 +23,11 @@ export class RiskProfileComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+  goToNext(form) {
+    this.portfolioService.setPortfolioRecommendationModalCounter(0);
+    this.router.navigate([PORTFOLIO_ROUTE_PATHS.PORTFOLIO_RECOMMENDATION])
+
   }
 
 }
