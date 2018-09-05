5d9cbacaafccff8e22c74f8b1a7651f9e84c8486
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-09-05 12:41:03 +0530

    unit text cases added

--- src/app/guide-me/insurance-results/existing-coverage-modal/existing-coverage-modal.component.spec.ts
+++ src/app/guide-me/insurance-results/existing-coverage-modal/existing-coverage-modal.component.spec.ts
@@ -1,16 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { GuideMeService } from '../../guide-me.service';
 import { ExistingCoverageModalComponent } from './existing-coverage-modal.component';
 
 describe('ExistingCoverageModalComponent', () => {
   let component: ExistingCoverageModalComponent;
   let fixture: ComponentFixture<ExistingCoverageModalComponent>;
-
+  let guideMeService: GuideMeService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ExistingCoverageModalComponent ]
     })
     .compileComponents();
+    guideMeService = TestBed.bind(GuideMeService);
   }));
 
   beforeEach(() => {
@@ -22,4 +24,24 @@ describe('ExistingCoverageModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('life protection field validity', () => {
+    const life_protection = component.existingCoverageForm.controls['LIFE_PROTECTION'];
+    expect(life_protection.valid).toBeFalsy();
+  });
+
+  it('life protection field validity', () => {
+    const life_protection = component.existingCoverageForm.controls['CRITICAL_ILLNESS'];
+    expect(life_protection.valid).toBeFalsy();
+  });
+
+  it('life protection field validity', () => {
+    const occupational = component.existingCoverageForm.controls['OCCUPATION_DISABILITY'];
+    expect(life_protection.valid).toBeFalsy();
+  });
+
+  it('life protection field validity', () => {
+    const life_protection = component.existingCoverageForm.controls['LONG_TERM_CARE'];
+    expect(life_protection.valid).toBeFalsy();
+  });
+
 });
