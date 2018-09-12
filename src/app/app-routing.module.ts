045bf6dd8536630fe660b2ce5c8ef4732f86fd51
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-09-12 18:14:09 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { Route } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlSegment, UrlSegmentGroup } from '@angular/router';
+import { CallBackComponent } from 'src/app/call-back/call-back.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'direct', pathMatch: 'full'},
   {path: 'direct', loadChildren: './direct/direct.module#DirectModule'},
   {path: 'guideme', loadChildren: './guide-me/guide-me.module#GuideMeModule'},
   {path: 'portfolio', loadChildren: './portfolio/portfolio.module#PortfolioModule'},
+  // {component: CallBackComponent, matcher: test},
   {path: 'signup', loadChildren: './sign-up/sign-up.module#SignUpModule'}
 ];
 
@@ -14,3 +17,17 @@ const routes: Routes = [
   declarations: []
 })
 export class AppRoutingModule { }
+
+// MyInfo changes
+// export function test(url: UrlSegment[], group: UrlSegmentGroup, route: Route) {
+//   if (window.location.pathname.indexOf('callback') > -1) {
+//     if (!url[0]) {
+//       url.push(new UrlSegment(window.location.pathname.split('/')[1], {data: window.location.search}));
+//     } else {
+//       url[0].path = window.location.pathname;
+//       url[0].path = url[0].path.split('/')[1];
+//       url[0].parameters = {data: window.location.search};
+//     }
+//   }
+//   return ({consumed: url});
+// }
