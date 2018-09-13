5056643e88fc563326b31c9e0589fd3d23afcc82
Jawahar G <jawaharg@hcl.com>
2018-09-13 12:26:11 +0530

    Removed duplicate function

--- src/app/shared/http/api.service.ts
+++ src/app/shared/http/api.service.ts
@@ -27,7 +27,6 @@ export class ApiService {
     private guideMeService: GuideMeService,
     private httpClient: HttpClient) { }
 
-
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
@@ -99,22 +98,6 @@ export class ApiService {
           return throwError('Something bad happened; please try again later.');
         })
       );
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
   }
 
   getProtectionNeedsList(userInfoForm) {
