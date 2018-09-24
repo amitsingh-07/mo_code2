3b4e07a8e37ef8956c1e0a026d6f61e76de53177
rajasekar <rajasekar-s@hcl.com>
2018-09-24 15:36:15 +0530

    employment details files added

---
+++ src/app/investment-account/employment-details/employment-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+
+import { HeaderService } from '../../shared/header/header.service';
+
+@Component({
+  selector: 'app-employment-details',
+  templateUrl: './employment-details.component.html',
+  styleUrls: ['./employment-details.component.scss']
+})
+export class EmploymentDetailsComponent implements OnInit {
+  pageTitle: string;
+
+  constructor(
+    public readonly translate: TranslateService,
+    public headerService: HeaderService,
+    private modal: NgbModal ) {
+    this.translate.use('en');
+    this.translate.get('COMMON').subscribe((result: string) => {
+      this.pageTitle = this.translate.instant('EMPLOYMENT_DETAILS.TITLE');
+      this.setPageTitle(this.pageTitle);
+    });
+  }
+
+  setPageTitle(title: string) {
+    this.headerService.setPageTitle(title);
+  }
+
+  ngOnInit() {
+  }
+
+}
