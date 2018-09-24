99b63602c6350f69872ec99ed9936f74bd12fa4b
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-09-24 20:51:09 +0530

    unit test cases updated for expenses, income and my-assets.

--- src/app/guide-me/my-assets/my-assets.component.spec.ts
+++ src/app/guide-me/my-assets/my-assets.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MyAssetsComponent } from './my-assets.component';
 
 describe('MyAssetsComponent', () => {
   let component: MyAssetsComponent;
   let fixture: ComponentFixture<MyAssetsComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let httpClientSpy: { get: jasmine.Spy };
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyAssetsComponent ]
+      imports: [ HttpClientModule, ReactiveFormsModule , NgbModule.forRoot()],
+      declarations: [ MyAssetsComponent ],
+      providers: [{ provide: Router, useValue: mockRouter }]
     })
     .compileComponents();
   }));
@@ -16,10 +30,69 @@ describe('MyAssetsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MyAssetsComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('form'));
+    de = fixture.debugElement.query(By.css('button'));
+    el = de.nativeElement;
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create assets component', () => {
     expect(component).toBeTruthy();
   });
+  it('should be invalid if form is empty', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+  });
+
+  it('validity of form cash', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+    component.assetsForm.controls['cash'].setValue(1000);
+    expect(component.assetsForm.valid).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:no-identical-functions
+  it('validity of form cpf', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+    component.assetsForm.controls['cpf'].setValue(500);
+    expect(component.assetsForm.valid).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:no-identical-functions
+  it('validity of form homeProperty', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+    component.assetsForm.controls['homeProperty'].setValue(100);
+    expect(component.assetsForm.valid).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:no-identical-functions
+  it('validity of form investmentProperties', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+    component.assetsForm.controls['investmentProperties'].setValue(700);
+    expect(component.assetsForm.valid).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:no-identical-functions
+  it('validity of form otherInvestments', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+    component.assetsForm.controls['otherInvestments'].setValue(500);
+    expect(component.assetsForm.valid).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:no-identical-functions
+  it('validity of form otherAssets', () => {
+    expect(component.assetsForm.valid).toBeFalsy();
+    component.assetsForm.controls['otherAssets'].setValue(100);
+    expect(component.assetsForm.valid).toBeTruthy();
+  });
+
+  // tslint:disable-next-line:no-identical-functions
+  it('testing the proceed button', async(() => {
+    spyOn(component, 'goToNext');
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(component.goToNext).toHaveBeenCalled();
+    });
+  }));
+
 });
