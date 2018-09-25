8b6035350eed7ec0cf457994aa96e3ee21b70ad0
rajasekar <rajasekar-s@hcl.com>
2018-09-25 18:25:45 +0530

    Nationality API Token Fix

--- src/app/investment-account/select-nationality/select-nationality.component.ts
+++ src/app/investment-account/select-nationality/select-nationality.component.ts
@@ -3,10 +3,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
+
 import { HeaderService } from '../../shared/header/header.service';
-import { ModelWithButtonComponent } from '../../shared/modal/model-with-button/model-with-button.component';
+import { AuthenticationService } from '../../shared/http/auth/authentication.service';
+import {
+    ModelWithButtonComponent
+} from '../../shared/modal/model-with-button/model-with-button.component';
 import { INVESTMENT_ACCOUNT_ROUTE_PATHS } from '../investment-account-routes.constants';
 import { InvestmentAccountService } from '../investment-account-service';
+
 @Component({
     selector: 'app-select-nationality',
     templateUrl: './select-nationality.component.html',
@@ -35,12 +40,13 @@ export class SelectNationalityComponent implements OnInit {
     modalTitle1: any;
     modalMessage1: any;
     countries: ['Singapore', 'India'];
-   constructor(
+    constructor(
         public headerService: HeaderService,
         public activeModal: NgbActiveModal,
         private router: Router,
         private investmentAccountService: InvestmentAccountService,
         private modal: NgbModal,
+        public authService: AuthenticationService,
         public readonly translate: TranslateService) {
         this.translate.use('en');
         this.translate.get('COMMON').subscribe(() => {
@@ -68,9 +74,12 @@ export class SelectNationalityComponent implements OnInit {
         this.singaporeQuestion = false;
     }
     getNationalityList() {
-        this.investmentAccountService.getNationalityList().subscribe((data) => {
-            this.nationalitylist = data.objectList;
-            console.log(this.nationalitylist);
+
+        this.authService.authenticate().subscribe((token) => {
+            this.investmentAccountService.getNationalityList().subscribe((data) => {
+                this.nationalitylist = data.objectList;
+                console.log(this.nationalitylist);
+            });
         });
 
     }
@@ -95,7 +104,7 @@ export class SelectNationalityComponent implements OnInit {
 
             this.selectNationalityFormValues = this.investmentAccountService.getNationality();
             this.selectNationalityForm = new FormGroup({
-            selectNationalitySingapore: new FormControl(this.selectNationalityFormValues.selectNationalitySingapore, Validators.required)
+                selectNationalitySingapore: new FormControl(this.selectNationalityFormValues.selectNationalitySingapore, Validators.required)
             });
             if (this.save(form)) {
                 if (form.controls.selectNationalitySingapore.value === 'yes') {
