22dc924e32d2dd6c9569a95ec44b7c4fac7a2239
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-09-29 03:01:53 +0530

    isNavbarCollapsed added

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent implements OnInit, AfterViewInit {
   showNavbar = true;
   showNavShadow = false;
   showSearchBar = false;
+  isNavbarCollapsed = false;
 
   innerWidth: any;
   mobileThreshold = 567;
