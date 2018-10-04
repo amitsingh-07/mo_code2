cf4de98cbeaed8cf14179b3df8b2c1a77811be42
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-10-04 17:12:25 +0530

    myinfo authorize url is set to environment file

--- src/app/shared/http/api.service.ts
+++ src/app/shared/http/api.service.ts
@@ -84,7 +84,7 @@ export class ApiService {
             console.error(
               `Backend returned code ${error.status}, ` + `body was: ${error.error}`
             );
-            return this.httpClient.get<IServerResponse>(url);
+            //return this.httpClient.get<IServerResponse>(url);
           }
           // return an observable with a user-facing error message
           return throwError('Something bad happened; please try again later.');
