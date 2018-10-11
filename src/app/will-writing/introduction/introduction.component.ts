b9f16c978fcdd6599bef6def57073cc2865304a5
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-10-11 12:52:11 +0530

    content moved to translate

--- src/app/will-writing/introduction/introduction.component.ts
+++ src/app/will-writing/introduction/introduction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-introduction',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroductionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    this.translate.use('en');
+   }
 
   ngOnInit() {
   }
