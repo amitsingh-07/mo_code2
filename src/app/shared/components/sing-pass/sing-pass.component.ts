8aa5c83618b21a7e4bce4847f225f58b493b4e04
Sakthi M <sakthi.m@hcl.com>
2018-10-16 13:38:36 +0530

    SingPass component addition

---
+++ src/app/shared/components/sing-pass/sing-pass.component.ts
@@ -0,0 +1,47 @@
+import { Router } from '@angular/router';
+import { InvestmentAccountService } from '../../../investment-account/investment-account-service';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { ErrorModalComponent } from '../../modal/error-modal/error-modal.component';
+import { MyInfoService } from '../../Services/my-info.service';
+
+@Component({
+  selector: 'app-sing-pass',
+  templateUrl: './sing-pass.component.html',
+  styleUrls: ['./sing-pass.component.scss']
+})
+export class SingPassComponent implements OnInit {
+  modelTitle: string;
+  modelMessge: string;
+
+  constructor(private modal: NgbModal,
+              private router: Router,
+              private myInfoService: MyInfoService,
+              public readonly translate: TranslateService,
+              private investmentAccountService: InvestmentAccountService
+            ) {
+    this.translate.use('en');
+    this.translate.get('COMMON').subscribe((result: string) => {
+      this.modelTitle = this.translate.instant('MYINFO.OPEN_MODAL_DATA.TITLE');
+      this.modelMessge = this.translate.instant('MYINFO.OPEN_MODAL_DATA.DESCRIPTION');
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  openModal() {
+    const ref = this.modal.open(ErrorModalComponent, { centered: true });
+    ref.componentInstance.errorTitle = this.modelTitle;
+    ref.componentInstance.errorMessage = this.modelMessge;
+    ref.componentInstance.isButtonEnabled = true;
+    ref.result.then(() => {
+      this.investmentAccountService.callBackInvestmentAccount = true;
+      this.myInfoService.setMyInfoAttributes(this.investmentAccountService.myInfoAttributes);
+      this.myInfoService.goToMyInfo();
+    }).catch((e) => {
+    });
+  }
+
+}
