75d12035e088fd71e2066c7952c717e18f9ea157
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-10-16 12:58:24 +0530

    my-estate-distribution component created.

---
+++ src/app/will-writing/my-estate-distribution/my-estate-distribution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-estate-distribution',
+  templateUrl: './my-estate-distribution.component.html',
+  styleUrls: ['./my-estate-distribution.component.scss']
+})
+export class MyEstateDistributionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
