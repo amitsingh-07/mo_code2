2acf65fed1b4c3e2412ccdaa0cd492ea440e8e57
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-10-17 14:27:28 +0530

    Routing added for static pages

--- src/app/will-writing/appoint-your-executor-trustee/appoint-your-executor-trustee.component.ts
+++ src/app/will-writing/appoint-your-executor-trustee/appoint-your-executor-trustee.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { WILL_WRITING_ROUTE_PATHS } from '../will-writing-routes.constants';
 
 @Component({
   selector: 'app-appoint-your-executor-trustee',
@@ -8,11 +11,17 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppointYourExecutorTrusteeComponent implements OnInit {
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private router: Router, private _location: Location) {
     this.translate.use('en');
   }
 
   ngOnInit() {
   }
 
+  goToNext() {
+    this.router.navigate([WILL_WRITING_ROUTE_PATHS.MY_BENEFICIARIES]);
+  }
+  goBack() {
+    this._location.back();
+  }
 }
