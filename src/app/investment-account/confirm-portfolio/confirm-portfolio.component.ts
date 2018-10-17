3ccaed758c635861b25179fb12dcabda898f0ce7
saidevi <Koganti.Devi@hcl.com>
2018-10-17 12:31:36 +0530

    Sprint5 merge

---
+++ src/app/investment-account/confirm-portfolio/confirm-portfolio.component.ts
@@ -0,0 +1,190 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavigationStart, Router } from '@angular/router';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+
+import {
+  BreakdownAccordionComponent
+} from '../../shared/components/breakdown-accordion/breakdown-accordion.component';
+import {
+  BreakdownBarComponent
+} from '../../shared/components/breakdown-bar/breakdown-bar.component';
+import { LoaderComponent } from '../../shared/components/loader/loader.component';
+import { HeaderService } from '../../shared/header/header.service';
+import { ErrorModalComponent } from '../../shared/modal/error-modal/error-modal.component';
+import {
+  ModelWithButtonComponent
+} from '../../shared/modal/model-with-button/model-with-button.component';
+import { NavbarService } from '../../shared/navbar/navbar.service';
+import { RegexConstants } from '../../shared/utils/api.regex.constants';
+import { INVESTMENT_ACCOUNT_ROUTE_PATHS } from '../investment-account-routes.constants';
+import { InvestmentAccountService } from '../investment-account-service';
+import { INVESTMENT_ACCOUNT_CONFIG } from '../investment-account.constant';
+import {
+  EditInvestmentModalComponent
+} from './edit-investment-modal/edit-investment-modal.component';
+
+@Component({
+  selector: 'app-confirm-portfolio',
+  templateUrl: './confirm-portfolio.component.html',
+  styleUrls: ['./confirm-portfolio.component.scss']
+})
+export class ConfirmPortfolioComponent implements OnInit {
+
+  uploadForm: FormGroup;
+  pageTitle: string;
+  formValues;
+  countries;
+  isUserNationalitySingapore;
+  defaultThumb;
+  showLoader;
+  loaderTitle;
+  loaderDesc;
+  formData: FormData = new FormData();
+  portfolio;
+
+  breakdownSelectionindex: number = null;
+  isAllocationOpen = false;
+
+  legendColors: string[] = ['#3cdacb', '#ec681c', '#76328e'];
+
+  constructor(
+    public readonly translate: TranslateService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    public headerService: HeaderService,
+    private modal: NgbModal,
+    public navbarService: NavbarService,
+    public investmentAccountService: InvestmentAccountService) {
+    this.translate.use('en');
+    this.translate.get('COMMON').subscribe((result: string) => {
+      this.pageTitle = this.translate.instant('CONFIRM_PORTFOLIO.TITLE');
+      this.setPageTitle(this.pageTitle);
+    });
+  }
+
+  setPageTitle(title: string) {
+    this.navbarService.setPageTitle(title);
+  }
+
+  ngOnInit() {
+    this.navbarService.setNavbarMobileVisibility(true);
+    this.navbarService.setNavbarMode(2);
+    this.isUserNationalitySingapore = this.investmentAccountService.isUserNationalitySingapore();
+
+    this.portfolio = {
+      "projectedValue": 112.0,
+      "portfolioId": "PORTFOLIO00057",
+      "tenure": "50",
+      "projectedReturns": "+5.50%",
+      "breakDown": [{
+        "id": "SECTOR00012",
+        "name": "Emerging Markets Equity",
+        "type": "Equities",
+        "riskRating": 9.0,
+        "totalPercentage": 60,
+        "funds": [{
+          "id": "FI3018",
+          "name": "Fidelity ASEAN A SGD",
+          "type": "UT",
+          "percentage": 60,
+          "factSheetLink": "http://",
+          "htmlDesc": null
+        }]
+      }, {
+        "id": "SECTOR00013",
+        "name": "Global Bonds",
+        "type": "Bonds",
+        "riskRating": 9.0,
+        "totalPercentage": 40,
+        "funds": [{
+          "id": "FI3018",
+          "name": "Fidelity ASEAN A SGD",
+          "type": "UT",
+          "percentage": 20,
+          "factSheetLink": "http://",
+          "htmlDesc": null
+        }, {
+          "id": "FI3018",
+          "name": "Fidelity ASEAN A SGD",
+          "type": "UT",
+          "percentage": 20,
+          "factSheetLink": "http://",
+          "htmlDesc": null
+        }]
+      }]
+    };
+  }
+
+  getInlineErrorStatus(control) {
+    return (!control.pristine && !control.valid);
+  }
+
+  setNestedDropDownValue(key, value, nestedKey) {
+    this.uploadForm.controls[nestedKey]['controls'][key].setValue(value);
+  }
+
+  markAllFieldsDirty(form) {
+    Object.keys(form.controls).forEach((key) => {
+      if (form.get(key).controls) {
+        Object.keys(form.get(key).controls).forEach((nestedKey) => {
+          form.get(key).controls[nestedKey].markAsDirty();
+        });
+      } else {
+        form.get(key).markAsDirty();
+      }
+    });
+  }
+
+  selectAllocation(event) {
+    if ((!this.isAllocationOpen)) {
+      this.breakdownSelectionindex = event;
+      this.isAllocationOpen = true;
+    } else {
+      if (event !== this.breakdownSelectionindex) {
+        // different tab
+        this.breakdownSelectionindex = event;
+        this.isAllocationOpen = true;
+      } else {
+        // same tab click
+        this.breakdownSelectionindex = null;
+        this.isAllocationOpen = false;
+      }
+    }
+  }
+
+  showPortfolioAssetModal() {
+      const errorTitle = this.translate.instant('CONFIRM_PORTFOLIO.MODAL.PORTFOLIO_ASSETS.TITLE');
+      const errorMessage = this.translate.instant('CONFIRM_PORTFOLIO.MODAL.PORTFOLIO_ASSETS.MESSAGE');
+      const ref = this.modal.open(ModelWithButtonComponent, { centered: true });
+      ref.componentInstance.imgType = 2;
+      ref.componentInstance.errorTitle = errorTitle;
+      ref.componentInstance.errorMessageHTML = errorMessage;
+  }
+
+  openEditInvestmentModal() {
+    const ref = this.modal.open(EditInvestmentModalComponent, {
+      centered: true
+    });
+    ref.componentInstance.investmentData = {
+      investmentPeriod: 5,
+      oneTimeInvestment: 12000,
+      monthlyInvestment: 1000
+    };
+    ref.componentInstance.modifiedInvestmentData.subscribe((emittedValue) => {
+      // update form data
+      ref.close();
+    });
+    this.dismissPopup(ref);
+  }
+
+  dismissPopup(ref: NgbModalRef) {
+    this.router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        ref.close();
+      }
+    });
+  }
+
+}
---
+++ src/assets/articles/13.jsp
@@ -0,0 +1,34 @@
+<img src="./../../../assets/articles/images/13/thumbnail.jpg" class="pull-right" style=" padding:10px;">
+<p>If you've had a look at the insurance products we feature on our comparison engine, you may have noticed that we do not feature <a href="http://www.moneysense.gov.sg/understanding-financial-products/investments/types-of-investments/investment-linked-insurance-policies.aspx" target="_blank" title="Investment Linked Policies">Investment-Linked Policies (ILPs)</a> at all. There's a very good reason why we don't advocate them. Read on to find out.</p>
+<br>
+
+<h3 class="primary-color"><strong>How do ILPs work?</strong></h3>
+<p>ILPs are insurance policies that combine life insurance coverage with investments into unit trusts. Part of your premiums goes to pay for insurance charges that pay for the cost of insurance benefits, and part of it is used for investing. The investment value is liquid and it can be withdrawn from anytime.</p>
+<p>For a regular premium ILP, the policyholder determines the annual premium amount, which stays level, as well as the percentage allocations to insurance charges and investments. However, ILPs are structured such that the insurance charges increase over time, which means that the remaining portion of the premiums that goes into investments shrinks over time.</p>
+<br>
+
+<h3 class="primary-color"><strong>The Problem With ILPs</strong></h3>
+<p>Insurance cost tends to increase especially sharply in the older years, and typically around age 65, it could well become higher than the annual premiums, upon which the <a href="http://www.moneysense.gov.sg/understanding-financial-products/investments/types-of-investments/investment-linked-insurance-policies.aspx" target="_blank" title="Investment Linked Policies">ILP</a> will dip into the investment value to cover the shortfall. You have the option to top-up the premiums to support the coverage, however if you choose not to it is technically possible that the investment value gets fully depleted, resulting in lower coverage or worse, policy lapse. Furthermore, the investment value is not guaranteed as it is dependent on the performance of the underlying funds you have chosen. This is why it is important to monitor their performance and make changes where required.</p>
+
+<p>What this means is that you may be subjecting the protection purpose of insurance to risk. Do you really want to have to wonder whether you will lose your coverage at a time when you cannot afford to?   
+
+If that doesn't convince you, here are a few more reasons:</p>
+<br>
+<h3 class="primary-color"><strong>If Your Main Purpose is Investment, It Does Not Adequately Fulfill the Investment Goal</strong></h3>
+<p>When you first start off, sure, the investment value is growing. But as the years pass, less of your premiums are going into investments because of the increasing insurance charge. Once the insurance charges become higher than the annual premiums the investment value will be shrinking instead, and in the worst case scenario, fully depleted. While you can choose to liquidate the investments, that would put you at higher risk of losing all or part of your life coverage.</p>
+
+<p>In addition, you are limited to the investment funds made available by the insurance company. If you were to invest outside of an ILP however, you will have a much wider range of funds from which to select the best of breeds.</p>
+<br>
+<h3 class="primary-color"><strong>If Your Main Purpose is Getting Protection, There Are More Cost Effective Alternatives</strong></h3>
+<p>With term insurance, you are paying premiums solely for the insurance charges without giving the insurance company extra to invest, which is why it is much cheaper. For example, it is possible for a 30-year-old male non-smoker to get $1,600,000 cover till age 65 with an annual premium of less than $2000, compared to as much as $700,000 cover with the same premium amount for an <a href="http://www.moneysense.gov.sg/understanding-financial-products/investments/types-of-investments/investment-linked-insurance-policies.aspx" target="_blank" title="Investment Linked Policies">ILP</a>. We cannot emphasise this enough: term insurance is the only way you can get adequate cover affordably. </p>
+<br>
+<h3 class="primary-color"><strong>If You're Trying to Achieve Both Purposes, It Gets Complicated</strong></h3>
+<p>On the one hand you want to ensure adequate life coverage, on the other you want to ensure returns on your investment. In order to achieve both you may have to pay huge premiums, which would cause a strain on your finances. </p>
+
+<p>In addition, any changes you make to the policy will impact both objectives, rendering decision-making tricky and often conflicting. This is why we believe firmly in keeping it simple: use term insurance to cover your protection needs, and use investment instruments (such as unit trusts) to invest your monies. Give yourself the flexibility to make decisions about one objective without worrying about the other. </p>
+
+<p>ILPs' claim to kill two birds with one stone certainly has its appeal. However, we've shown that there's a very real possibility that you might end up achieving neither the protection nor the investment objective, so it's better to keep them separate. </p>  
+<br>
+<div class="text-xs-center"><a href="../direct/savings">
+  <img src="./../../../assets/images/banners/compare_sav_540w.png" alt="compare savings plans"></a>
+</div>
