cf2ba97b2dced55600492a2230506bfa42281ac2
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-10-23 15:39:49 +0530

    Fixed: BFA-930

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,17 @@
 import { isDebugMode } from '../app/shared/logger/console-logger.service';
 import { IEnvironment } from './environment.interface';
-
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment: IEnvironment = {
-  production: false,
-  isDebugMode: true,
-  apiBaseUrl: 'https://bfa-dev.ntucbfa.cloud',
-  myInfoClientId: 'STG2-MYINFO-SELF-TEST',
-  myInfoCallbackBaseUrl: 'https://localhost:3001',
+  production: true,
+  isDebugMode: false,
+  apiBaseUrl: 'https://bfa-uat.ntucbfa.com',
+  myInfoClientId: 'STG-201820691C-MONEYOWL-BFA',
+  myInfoCallbackBaseUrl: 'https://bfa-uat.ntucbfa.com/myinfo',
   myInfoAuthorizeUrl: 'https://myinfosgstg.api.gov.sg/test/v2/authorise'
 };
-
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
