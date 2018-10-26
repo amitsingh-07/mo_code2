f3a17c36c02b88d4e237c27e427936e4c4002164
satheesan <satheesan.o@hcl.com>
2018-10-26 19:09:25 +0530

    Edit profile updated.

--- src/app/sign-up/edit-profile/edit-profile.component.ts
+++ src/app/sign-up/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
@@ -7,19 +8,23 @@ import { NavbarService } from '../../shared/navbar/navbar.service';
 import { RegexConstants } from '../../shared/utils/api.regex.constants';
 import { SignUpApiService } from '../sign-up.api.service';
 import { SIGN_UP_ROUTE_PATHS } from '../sign-up.routes.constants';
+import { SignUpService } from '../sign-up.service';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.scss']
 })
 export class EditProfileComponent implements OnInit {
-
+  resetPasswordForm: FormGroup;
+  formValues: any;
   constructor(
     // tslint:disable-next-line
+    private formBuilder: FormBuilder,
     private modal: NgbModal,
     public headerService: HeaderService,
     public navbarService: NavbarService,
     private signUpApiService: SignUpApiService,
+    private signUpService: SignUpService,
     private route: ActivatedRoute,
     private router: Router,
     private translate: TranslateService) {
@@ -31,6 +36,21 @@ export class EditProfileComponent implements OnInit {
   ngOnInit() {
     this.navbarService.setNavbarMobileVisibility(true);
     this.navbarService.setNavbarMode(1);
+    this.buildForgotPasswordForm();
   }
-
+  showHidePassword(el) {
+    if (el.type === 'password') {
+      el.type = 'text';
+    } else {
+      el.type = 'password';
+    }
+  }
+  buildForgotPasswordForm() {
+    this.formValues = this.signUpService.getForgotPasswordInfo();
+    this.resetPasswordForm = this.formBuilder.group({
+      oldPassword: [this.formValues.oldPassword, [Validators.required,  Validators.pattern(RegexConstants.Password.Full)]],
+      newPassword: [this.formValues.oldPassword, [Validators.required,  Validators.pattern(RegexConstants.Password.Full)]],
+      confirmPassword: [this.formValues.oldPassword, [Validators.required,  Validators.pattern(RegexConstants.Password.Full)]]
+    });
+  }
 }
