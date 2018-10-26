b464f66e6f1c3488bcab551cd52494a1588bb4ff
satheesan <satheesan.o@hcl.com>
2018-10-26 14:24:55 +0530

    Edit profile updated

--- src/app/sign-up/edit-profile/edit-profile.component.ts
+++ src/app/sign-up/edit-profile/edit-profile.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { HeaderService } from '../../shared/header/header.service';
+import { NavbarService } from '../../shared/navbar/navbar.service';
+import { RegexConstants } from '../../shared/utils/api.regex.constants';
+import { SignUpApiService } from '../sign-up.api.service';
+import { SIGN_UP_ROUTE_PATHS } from '../sign-up.routes.constants';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
@@ -7,9 +14,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    // tslint:disable-next-line
+    private modal: NgbModal,
+    public headerService: HeaderService,
+    public navbarService: NavbarService,
+    private signUpApiService: SignUpApiService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private translate: TranslateService) {
+    this.translate.use('en');
+    this.route.params.subscribe((params) => {
+    });
+  }
 
   ngOnInit() {
+    this.navbarService.setNavbarMobileVisibility(true);
+    this.navbarService.setNavbarMode(1);
   }
 
 }
