cd0273ac1d858a4a0ad1b1e1bd96e10b1f360283
Vetrivel Gunasekaran <vetrivel.g@hcl.com>
2018-11-07 20:22:57 +0530

    MyInfo API changes

--- src/app/shared/http/api.service.ts
+++ src/app/shared/http/api.service.ts
@@ -168,25 +168,9 @@ export class ApiService {
   }
 
   getMyInfoData(data) {
-    const url = '../assets/mock-data/myInfoValues.json';
-    return this.http.post(apiConstants.endpoint.getMyInfoValues, data.code, true)
+    return this.http.post(apiConstants.endpoint.getMyInfoValues, data, true)
       .pipe(
-        // tslint:disable-next-line:no-identical-functions
-        catchError((error: HttpErrorResponse) => {
-          if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
-          } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            console.error(
-              `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-            );
-            return this.httpClient.get<IServerResponse>(url);
-          }
-          // return an observable with a user-facing error message
-          return throwError('Something bad happened; please try again later.');
-        })
+        catchError((error: HttpErrorResponse) => this.handleError(error))
       );
   }
 
