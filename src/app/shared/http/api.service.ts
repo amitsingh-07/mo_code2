cd8fd29bf85daee16f1450335946f3e6846fac78
Manikantudu Karusala <manikantudu.k@hcl.com>
2018-11-09 18:08:36 +0530

    resolving conflicts

--- src/app/shared/http/api.service.ts
+++ src/app/shared/http/api.service.ts
@@ -778,45 +778,20 @@ export class ApiService {
       );
   }
 
-  // // Verify PromoCode
-  // verifyPromoCode(promoCode) {
-  //   return this.http.post(apiConstants.endpoint.verifyPromoCode, promoCode)
-  //   .pipe(
-  //     catchError((error: HttpErrorResponse) => this.handleError(error))
-  //   );
-  // }
+   // Verify PromoCode
+   verifyPromoCode(promoCode) {
+     return this.http.post(apiConstants.endpoint.verifyPromoCode, promoCode)
+     .pipe(
+       catchError((error: HttpErrorResponse) => this.handleError(error))
+     );
+   }
 
-  // // Get PromoCode
-  // getPromoCode() {
-  //   return this.http.get(apiConstants.endpoint.getPromoCode)
-  //     .pipe(
-  //       catchError((error: HttpErrorResponse) => this.handleError(error))
-  //     );
-  // }
-
-
-  // Verify PromoCode
-  verifyPromoCode(promoCode) {
-    const url = '../assets/mock-data/validatePromo.json';
-    return this.http.post(apiConstants.endpoint.verifyPromoCode, promoCode)
-      .pipe(
-        // tslint:disable-next-line:no-identical-functions
-        catchError((error: HttpErrorResponse) => {
-          if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
-          } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            console.error(
-              `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-            );
-            return this.httpClient.get<IServerResponse>(url);
-          }
-          // return an observable with a user-facing error message
-          return throwError('Something bad happened; please try again later.');
-        })
-      );
-  }
+   // Get PromoCode
+   getPromoCode() {
+     return this.http.get(apiConstants.endpoint.getPromoCode)
+       .pipe(
+         catchError((error: HttpErrorResponse) => this.handleError(error))
+       );
+   }
 
 }
