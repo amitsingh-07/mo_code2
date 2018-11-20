a12d33bc966f2a80e5425845afd5f92657a86257
rajasekar <rajasekar-s@hcl.com>
2018-11-20 15:51:16 +0530

    Fund Details interface added

---
+++ src/app/investment-account/fund-your-account/fund-details.ts
@@ -0,0 +1,9 @@
+export class FundDetails {
+    oneTimeInvestment: number; // Funding
+    monthlyInvestment: number; // Funding
+    investmentAmount: number; // Topup
+    fundingAmount: number; // Topup
+    fundingType: string;
+    source: string;
+    portfolio: string;
+}
