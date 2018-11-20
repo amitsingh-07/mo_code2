9c0ef00820f479a17124f6ce360b1694aa9016b9
Ajay Maharana <ajayakumar.m@hcl.com>
2018-11-20 18:27:28 +0530

    Transaction History Tab and Accordion Functionality

--- src/app/portfolio/transaction/transaction.component.ts
+++ src/app/portfolio/transaction/transaction.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.scss']
+  styleUrls: ['./transaction.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TransactionComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
-
 }
+//export class NgbdTabsetBasic { }
+
+
+
