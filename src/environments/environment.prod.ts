35c49aa2b5cd6ffa09de8d186baee21bbe8a6a99
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-11-22 17:51:04 +0530

    API base url updated.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: true,
   isDebugMode: false,
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://moneyowl.com.sg',
   myInfoClientId: 'PROD-201820691C-MONEYOWL-BFA',
   myInfoCallbackBaseUrl: 'https://moneyowl.com.sg/myinfo',
   myInfoAuthorizeUrl: 'https://myinfosg.api.gov.sg/v2/authorise',
