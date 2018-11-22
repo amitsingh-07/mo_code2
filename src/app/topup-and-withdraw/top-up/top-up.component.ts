28b5437313c67dbef00483616c0a64feef543816
saidevi <Koganti.Devi@hcl.com>
2018-11-22 09:28:50 +0530

    integrate the portfolio API in top up.

--- src/app/topup-and-withdraw/top-up/top-up.component.ts
+++ src/app/topup-and-withdraw/top-up/top-up.component.ts
@@ -6,13 +6,13 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
 
 import {
-    INVESTMENT_ACCOUNT_ROUTE_PATHS
+  INVESTMENT_ACCOUNT_ROUTE_PATHS
 } from '../../investment-account/investment-account-routes.constants';
 import { HeaderService } from '../../shared/header/header.service';
 import { AuthenticationService } from '../../shared/http/auth/authentication.service';
 import { ErrorModalComponent } from '../../shared/modal/error-modal/error-modal.component';
 import {
-    ModelWithButtonComponent
+  ModelWithButtonComponent
 } from '../../shared/modal/model-with-button/model-with-button.component';
 import { NavbarService } from '../../shared/navbar/navbar.service';
 import { RegexConstants } from '../../shared/utils/api.regex.constants';
@@ -90,8 +90,8 @@ export class TopUpComponent implements OnInit {
     // this.buildFormInvestment();
   }
   getPortfolioList() {
-    this.topupAndWithDrawService.getPortfolioList().subscribe((data) => {
-      this.portfolioList = data.objectList;
+    this.topupAndWithDrawService.getAllDropDownList().subscribe((data) => {
+      this.portfolioList = data.objectList.riskProfileTypes;
       console.log(this.portfolioList);
     });
   }
