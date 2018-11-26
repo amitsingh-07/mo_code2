f01df27950ebe48ceaa5c0df82e515c73bb64df7
MONEYOWL/weihao <weihao@moneyowl.com.sg>
2018-11-26 16:45:19 +0800

    -Implementing Missing Translate Modules - Adding seoService for meta and title settings - Adding all the modules under config settings

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,27 +1,47 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { ArticleCategoryComponent } from './article-category/article-category.component';
 import { ArticleEntryComponent } from './article-entry/article-entry.component';
 import { ArticleRoutingModule } from './article-routing.module';
 import { ArticleComponent } from './article/article.component';
 
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+import { SubscribeSideComponent } from './../shared/components/subscribe-side/subscribe-side.component';
 import { StringToLinkFormatPipe } from './../shared/Pipes/string-to-link.pipe';
 
-import { SubscribeSideComponent } from './../shared/components/subscribe-side/subscribe-side.component';
+export function createTranslateLoader(http: HttpClient) {
+  return new MultiTranslateHttpLoader(
+    http,
+    [
+      { prefix: './assets/i18n/app/', suffix: '.json' },
+      { prefix: './assets/i18n/articles/', suffix: '.json' },
+      { prefix: './assets/i18n/error/', suffix: '.json' }
+    ]);
+}
 
 @NgModule({
   imports: [
     CommonModule,
     ArticleRoutingModule,
-    NgbModule,
+    NgbModule.forRoot(),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
    ],
   declarations: [ArticleComponent, ArticleCategoryComponent,
     ArticleEntryComponent, SubscribeSideComponent,
     StringToLinkFormatPipe]
 })
+
 export class ArticleModule { }
