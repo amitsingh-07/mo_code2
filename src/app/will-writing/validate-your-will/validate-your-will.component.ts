1e2ceb1f60874198ddf943e24ff43f74a55bed67
Vetrivel Gunasekaran <vetrivel.g@hcl.com>
2018-11-28 17:03:05 +0530

    Download will changes for other browsers

--- src/app/will-writing/validate-your-will/validate-your-will.component.ts
+++ src/app/will-writing/validate-your-will/validate-your-will.component.ts
@@ -44,9 +44,30 @@ export class ValidateYourWillComponent implements OnInit, OnDestroy {
 
   downloadWill() {
     this.willWritingApiService.downloadWill().subscribe((data: any) => {
-      this.downloadFile(data);
+      this.saveAs(data);
     }, (error) => console.log(error));
   }
+
+  saveAs(data) {
+    const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const otherBrowsers = /Android|Windows/.test(navigator.userAgent);
+
+    const blob = new Blob([data], { type: 'application/pdf' });
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob, 'Will writing.pdf');
+    } else if (isSafari && iOS || otherBrowsers) {
+      this.downloadFile(data);
+    } else {
+      const reader: any = new FileReader();
+      const out = new Blob([data], { type: 'application/pdf' });
+      reader.onload = ((e) => {
+        window.open(reader.result);
+      });
+      reader.readAsDataURL(out);
+    }
+  }
+
   downloadFile(data: any) {
     const blob = new Blob([data], { type: 'application/pdf' });
     const url = window.URL.createObjectURL(blob);
@@ -54,7 +75,7 @@ export class ValidateYourWillComponent implements OnInit, OnDestroy {
     document.body.appendChild(a);
     a.setAttribute('style', 'display: none');
     a.href = url;
-    a.download = 'MoneyOwl Will Writing';
+    a.download = 'Will Writing.pdf';
     a.click();
     window.URL.revokeObjectURL(url);
     a.remove();
