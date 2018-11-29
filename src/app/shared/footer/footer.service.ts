ac7b9836b406f3506a08652f98c58c19c4b835a4
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-11-29 17:13:56 +0530

    Fixed: maintenance message in footer

--- src/app/shared/footer/footer.service.ts
+++ src/app/shared/footer/footer.service.ts
@@ -1,25 +1,18 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { ConfigService, IConfig } from './../../config/config.service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class FooterService {
 
   private footerVisibility = new BehaviorSubject(false);
-  isMaintenanceEnabled: boolean;
   currentFooterVisibility = this.footerVisibility.asObservable();
 
-  constructor(private configService: ConfigService) {
-    this.configService.getConfig().subscribe((config: IConfig) => {
-      this.isMaintenanceEnabled = config.maintenanceEnabled;
-      console.log(this.isMaintenanceEnabled);
-    });
+  constructor() {
   }
 
   setFooterVisibility(isVisible: boolean) {
     this.footerVisibility.next(isVisible);
-    }
+  }
 }
