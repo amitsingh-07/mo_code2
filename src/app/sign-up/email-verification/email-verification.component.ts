c56616a26ad1fe3ee32c76220d179ffbd0e89588
Vinothkumar Arputharaj <vinothkumara@ntuclink.com.sg>
2018-11-29 17:35:07 +0530

    Adding - Account create success message, login btn in email verification

--- src/app/sign-up/email-verification/email-verification.component.ts
+++ src/app/sign-up/email-verification/email-verification.component.ts
@@ -1,3 +1,4 @@
+import { ConfigService, IConfig } from './../../config/config.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
@@ -21,7 +22,7 @@ export class EmailVerificationComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private translate: TranslateService,
-    private authService: AuthenticationService
+    private authService: AuthenticationService, private configService: ConfigService
   ) {
     this.translate.use('en');
   }
