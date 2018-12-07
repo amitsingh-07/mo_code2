46ad7e013e38cebf8df4638c37e1bfd090bca0d3
rajasekar <rajasekar-s@hcl.com>
2018-12-07 18:32:19 +0530

    Util changes reverted

--- src/app/shared/utils/util.ts
+++ src/app/shared/utils/util.ts
@@ -7,12 +7,9 @@ export class Util {
     public static getApiBaseUrl(): string {
         let apiBaseUrl = '';
         if (window.location.href.indexOf('localhost') >= 0 || window.location.port === '4200') {
-            if ((window.location.href.indexOf('dashboard') >= 0) || (window.location.href.indexOf('view-notifications') >= 0) ) {
-                apiBaseUrl = 'https://bfa-dev.ntucbfa.cloud';
-            } else {
-                apiBaseUrl = environment.apiBaseUrl;
-            }
+            apiBaseUrl = environment.apiBaseUrl;
         }
+
         return apiBaseUrl;
     }
 }
