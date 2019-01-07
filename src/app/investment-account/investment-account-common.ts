d96bc38b05528e5feabee58eee594438b5c97b51
rajasekar <rajasekar-s@hcl.com>
2019-01-07 19:34:48 +0530

    MO2-714 Upload Documents - PDF Allowed.

--- src/app/investment-account/investment-account-common.ts
+++ src/app/investment-account/investment-account-common.ts
@@ -59,12 +59,11 @@ export class InvestmentAccountCommon {
     const fileSize: number = selectedFile.size / 1024 / 1024; // in MB
     const fileType = selectedFile.name.split('.')[1].toUpperCase();
     const isValidFileSize = fileSize <= INVESTMENT_ACCOUNT_CONFIG.upload_documents.max_file_size;
-    const isValidFileType = thumbElem ? INVESTMENT_ACCOUNT_CONFIG.upload_documents.image_file_types.includes(fileType)
-      : INVESTMENT_ACCOUNT_CONFIG.upload_documents.doc_file_types.includes(fileType);
+    const isValidFileType = INVESTMENT_ACCOUNT_CONFIG.upload_documents.file_types.includes(fileType);
     if (isValidFileSize && isValidFileType) {
       const payloadKey = this.getPayloadKey(controlname);
       formData.append(payloadKey, selectedFile);
-      if (thumbElem) {
+      if (fileType !== 'PDF') {
         this.setThumbnail(thumbElem, selectedFile);
       }
     }
--- src/app/investment-account/investment-account.constant.ts
+++ src/app/investment-account/investment-account.constant.ts
@@ -13,9 +13,8 @@ export let INVESTMENT_ACCOUNT_CONFIG = {
     },
     upload_documents: {
         default_thumb: 'cam-icon.svg',
-        max_file_size: 2, // in MB
-        image_file_types: ['JPG', 'JPEG', 'PNG', 'GIF', 'BMP'],
-        doc_file_types: ['PDF']
+        max_file_size: 10, // in MB
+        file_types: ['JPG', 'JPEG', 'PNG', 'GIF', 'BMP', 'PDF']
     },
     confirm_portfolio: {
         fees: {
