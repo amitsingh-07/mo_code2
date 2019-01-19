fb3129bc60091f0ba16d9b1fc98355bca8a1c3cb
Sakthi M <sakthimuthusamy@ntuclink.com.sg>
2019-01-19 16:39:26 +0530

    Build issue fix

--- src/environments/environment.project-g.ts
+++ src/environments/environment.project-g.ts
@@ -9,6 +9,7 @@ export const environment: IEnvironment = {
   production: false,
   isDebugMode: true,
   apiBaseUrl: '',
+  apiBaseUrl2: '',
   myInfoClientId: 'STG2-MYINFO-SELF-TEST',
   myInfoCallbackBaseUrl: 'https://localhost:3001',
   myInfoAuthorizeUrl: 'https://myinfosgstg.api.gov.sg/test/v2/authorise',
