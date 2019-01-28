b0f26b4898c8add268d48e265d733d4b6e38bfd2
saidevi <Koganti.Devi@hcl.com>
2019-01-28 15:47:38 +0530

    fixed the MO@-1099.

--- src/app/portfolio/personal-info/personal-form-error.ts
+++ src/app/portfolio/personal-info/personal-form-error.ts
@@ -36,6 +36,7 @@ export class PersonalFormError {
                         two: { errorTitle: 'Oops! Review Monthly<br>Investment Amount', errorMessage: 'We require a monthly investment of at least $50.' },
                         // tslint:disable-next-line:max-line-length
                         three: { errorTitle: 'Oops! Review One-time<br>Investment Amount', errorMessage: 'We require a one-time investment of at least $100.' },
+                        
                         four: { errorTitle: 'Invalid Amount', errorMessage: ' Must select at least 1 checkbox to continue.' }
                         }
         };
