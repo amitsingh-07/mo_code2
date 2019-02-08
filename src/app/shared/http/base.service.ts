cdc0b5a68d50310d3f45b31f5a8127f474b79439
Sakthi M <sakthimuthusamy@ntuclink.com.sg>
2019-02-08 18:22:30 +0530

    Investment - Delete portfolio API update

--- src/app/shared/http/base.service.ts
+++ src/app/shared/http/base.service.ts
@@ -108,10 +108,16 @@ export class BaseService {
       });
   }
 
-  delete(url, postBody: any) {
-    this.helperService.showLoader();
-    return this.http
-      .delete(url)
+  delete(url, postBody: any, showLoader?: boolean, showError?: boolean) {
+    if (showLoader) {
+      this.helperService.showLoader();
+    }
+    let param = '';
+    if (showError) {
+      param = '?alert=' + showError;
+    }
+    return this.httpClient
+      .delete(`${this.apiBaseUrl}/${url}${param}`)
       .map((res: Response) => {
         return this.handleResponse(res);
       })
