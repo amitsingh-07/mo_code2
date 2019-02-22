d889fe75234e6e8e589ac057c6ab57d6c41e42fd
Rajasekar <rajasekar-s@hcl.com>
2019-02-22 10:57:32 +0530

    Topup error messages - values populated from constants

--- src/app/topup-and-withdraw/top-up/top-up-form-error.ts
+++ src/app/topup-and-withdraw/top-up/top-up-form-error.ts
@@ -1,15 +1,18 @@
+import { PORTFOLIO_CONFIG } from '../../portfolio/portfolio.constants';
+
 export class TopUPFormError {
+
   formFieldErrors: object = {
     topupValidations: {
       // tslint:disable-next-line:no-duplicate-string
       zero: {
         errorTitle: 'Oops! Review Investment Amount',
-        errorMessage: 'We require a one-time investment of at least $100.'
+        errorMessage: 'We require a one-time investment of at least $' + PORTFOLIO_CONFIG.my_financials.min_initial_amount + '.'
       },
       // tslint:disable-next-line:max-line-length
       more: {
         errorTitle: 'Oops! Review Investment Amount',
-        errorMessage: 'We require a monthly investment of at least $50.'
+        errorMessage: 'We require a monthly investment of at least $' + PORTFOLIO_CONFIG.my_financials.min_monthly_amount + '.'
       }
       // tslint:disable-next-line:max-line-length
     }
