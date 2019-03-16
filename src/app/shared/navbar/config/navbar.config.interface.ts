dca56f2db892bc99a086df7e4bf5af9fd8380a58
Sakthi M <sakthimuthusamy@ntuclink.com.sg>
2019-03-16 23:46:58 +0530

    UAT1-2 Nav bar config

---
+++ src/app/shared/navbar/config/navbar.config.interface.ts
@@ -0,0 +1,14 @@
+export interface INavbarConfig {
+    showNavBackBtn: boolean;
+    showHeaderBackBtn: boolean;
+    showMenu: true;
+    showLogin: true;
+    showNavShadow: true;
+    showSearchBar: false;
+    showNotifications: boolean;
+    showHeaderNavbar: false;
+    showLabel ?: {
+        'primary': string;
+        'secondary' ?: string;
+        };
+    }
