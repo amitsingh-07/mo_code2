2330ed4395122f2ff72a16fb9b1b72e6bf32f642
Praveen NT <jawaharg@hcl.com>
2019-03-20 16:02:17 +0530

    education changes

--- src/app/comprehensive/dependant-education-selection/dependant-education-selection.component.ts
+++ src/app/comprehensive/dependant-education-selection/dependant-education-selection.component.ts
@@ -33,8 +33,6 @@ export class DependantEducationSelectionComponent implements OnInit, OnDestroy {
   educationPreference = true;
   menuClickSubscription: Subscription;
   summaryModalDetails: IMySummaryModal;
-  endowmentDetail: IChildEndowment[];
-  dependantsArray: IChildEndowment[];
   constructor(
     private route: ActivatedRoute, private router: Router, public navbarService: NavbarService,
     private translate: TranslateService, private formBuilder: FormBuilder,
@@ -78,10 +76,13 @@ export class DependantEducationSelectionComponent implements OnInit, OnDestroy {
 
     this.comprehensiveService.updateComprehensiveSummary();
     this.hasEndowments = this.comprehensiveService.hasEndowment();
-    this.endowmentDetail = this.comprehensiveService.getChildEndowment();
-    console.log(this.endowmentDetail);
-    if (this.endowmentDetail.length > 0) {
-      this.dependantsArray = this.endowmentDetail;
+    this.hasEndowments === '0' ? this.education_plan_selection = true : this.education_plan_selection = false;
+    this.childEndowmentArray = this.comprehensiveService.getChildEndowment();
+    this.dependantDetailsArray = this.comprehensiveService.getMyDependant();
+    console.log(this.childEndowmentArray);
+    if (this.childEndowmentArray.length > 0) {
+      this.buildChildEndowmentFormArray();
+      this.buildEducationSelectionForm();
     } else {
       this.dependantDetailsArray.forEach((dependant: IDependantDetail) => {
         if (dependant.relationship.toLowerCase() === 'child' || dependant.relationship.toLowerCase() === 'sibling') {
