4ee652bae8a0e7ea8a685043fadfda96c89e9b7f
saidevi <Koganti.Devi@hcl.com>
2019-03-26 12:09:46 +0530

    fixed the MO2-1499,MO2-1501

--- src/app/shared/directives/percentage-input.directive.ts
+++ src/app/shared/directives/percentage-input.directive.ts
@@ -14,6 +14,7 @@ export class PercentageInputDirective implements AfterViewInit {
         private el: ElementRef, private currencyPipe: CurrencyPipe,
         private control: NgControl,
         private decimalPipe: DecimalPipe) {
+            this.el.nativeElement.type = 'tel'; // workaround for predictive text keyboard issue in samsung devices
     }
     ngAfterViewInit() {
         this.formatCurrency();
