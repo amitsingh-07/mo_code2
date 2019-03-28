7ce61485232847225aea10b82d6ba988d0c76cf9
Praveen NT <jawaharg@hcl.com>
2019-03-28 11:40:42 +0530

    enable guard commented line

--- src/app/comprehensive/comprehensive-enable-guard.ts
+++ src/app/comprehensive/comprehensive-enable-guard.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import { ProgressTrackerUtil } from 'src/app/shared/modal/progress-tracker/progress-tracker-util';
 
 import { appConstants } from '../app.constants';
 import { AppService } from '../app.service';
 import { ConfigService, IConfig } from '../config/config.service';
+import { LoaderService } from '../shared/components/loader/loader.service';
 import { AuthenticationService } from '../shared/http/auth/authentication.service';
-import { ProgressTrackerUtil } from '../shared/modal/progress-tracker/progress-tracker-util';
 import { SignUpService } from '../sign-up/sign-up.service';
-import { LoaderService } from './../shared/components/loader/loader.service';
 import { SIGN_UP_ROUTE_PATHS } from './../sign-up/sign-up.routes.constants';
 import { ComprehensiveApiService } from './comprehensive-api.service';
-import { COMPREHENSIVE_BASE_ROUTE } from './comprehensive-routes.constants';
+import { COMPREHENSIVE_BASE_ROUTE, COMPREHENSIVE_ROUTE_PATHS } from './comprehensive-routes.constants';
 import { ComprehensiveService } from './comprehensive.service';
 
 @Injectable()
-export class ComprehensiveChildEnableGuard implements CanActivateChild {
+export class ComprehensiveEnableGuard implements CanActivate {
   isComprehensiveEnabled = false;
   constructor(
     private configService: ConfigService, private router: Router,
@@ -27,7 +26,7 @@ export class ComprehensiveChildEnableGuard implements CanActivateChild {
       this.isComprehensiveEnabled = config.comprehensiveEnabled;
     });
   }
-  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (!this.isComprehensiveEnabled) {
       this.router.navigate([appConstants.homePageUrl]);
       return false;
@@ -40,33 +39,8 @@ export class ComprehensiveChildEnableGuard implements CanActivateChild {
         this.signUpService.setRedirectUrl(state.url);
         this.router.navigate([SIGN_UP_ROUTE_PATHS.LOGIN]);
         return false;
-      } else if (!this.cmpService.getComprehensiveSummary().comprehensiveEnquiry
-        || !this.cmpService.getComprehensiveSummary().comprehensiveEnquiry.enquiryId) {
-        this.appService.setJourneyType(appConstants.JOURNEY_TYPE_COMPREHENSIVE);
-        this.loaderService.showLoader({title: 'Loading'});
-        return this.cmpApiService.getComprehensiveSummary().pipe(map((data) => {
-          this.cmpService.setComprehensiveSummary(data.objectList[0]);
-          const accessibleUrl1 = this.cmpService.getAccessibleUrl(state.url);
-          if (!ProgressTrackerUtil.compare(accessibleUrl1, state.url)) {
-            this.appService.setJourneyType(appConstants.JOURNEY_TYPE_COMPREHENSIVE);
-            const accessibleUrl3 = this.cmpService.getAccessibleUrl(state.url);
-            this.router.navigate([accessibleUrl3]);
-            return false;
-          } else {
-            return true;
-          }
-        }));
-      } else {
-        const accessibleUrl2 = this.cmpService.getAccessibleUrl(state.url);
-        if (!ProgressTrackerUtil.compare(accessibleUrl2, state.url)) {
-          this.appService.setJourneyType(appConstants.JOURNEY_TYPE_COMPREHENSIVE);
-          this.router.navigate([accessibleUrl2]);
-          return false;
-        } else {
-          return true;
-        }
       }
+      return true;
     }
   }
 }
-
