a2f50778c31f9cfe6289043abee783edc1a7f076
Jawahar G <jawaharg@hcl.com>
2019-03-29 17:24:06 +0530

    BFA-1395 - Reverted

--- src/app/shared/utils/crypto.ts
+++ src/app/shared/utils/crypto.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-import * as CryptoJS from 'crypto-js';
 import { sha512 } from 'js-sha512';
-import { AuthenticationService } from '../http/auth/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptoService {
-  constructor(private authService: AuthenticationService) { }
+  constructor() { }
 
-  encrypt(pwd: any) {
-    const secret = this.authService.getSessionId();
-    const password = sha512(pwd);
-    let encrypted = CryptoJS.AES.encrypt(password, secret);
-    return encrypted.toString();
+  encrypt(value: any) {
+    return sha512(value);
   }
 }
