4c9b477ad10b71836f8af941f3bce10998eccf93
Praveen NT <jawaharg@hcl.com>
2019-04-04 18:41:30 +0530

    MO3-713 ,MO3 714,MO3 -712, MO3-664 ,MY Info Integration calculation

--- src/app/comprehensive/insurance-plan/insurance-plan.component.ts
+++ src/app/comprehensive/insurance-plan/insurance-plan.component.ts
@@ -26,7 +26,7 @@ export class InsurancePlanComponent implements OnInit {
   insurancePlanForm: FormGroup;
   insurancePlanFormValues: IInsurancePlan;
   longTermInsurance = true;
-  haveHDB = true;
+  haveHDB = false;
   submitted = false;
   insurancePlanningDependantModal: any;
   insurancePlanningNonDependantModal: any;
