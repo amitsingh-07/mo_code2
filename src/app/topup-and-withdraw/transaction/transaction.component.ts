9f41c1d0db542f0ea8e02a195cd71b40ff16aa26
MONEYOWL/weihao <weihao@moneyowl.com.sg>
2019-04-05 14:10:18 +0800

    BFA-1455 - Additional Notifications and Logout to Post Investment Screens

--- src/app/topup-and-withdraw/transaction/transaction.component.ts
+++ src/app/topup-and-withdraw/transaction/transaction.component.ts
@@ -47,7 +47,7 @@ export class TransactionComponent implements OnInit {
   }
   ngOnInit() {
     this.navbarService.setNavbarMobileVisibility(true);
-    this.navbarService.setNavbarMode(6);
+    this.navbarService.setNavbarMode(103);
     this.footerService.setFooterVisibility(false);
     this.getTransactionHistory();
 
