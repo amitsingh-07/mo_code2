cac44de300321ba93ceb973e5fb7db7b9efa3dc6
Jawahar G <jawaharg@hcl.com>
2019-04-10 12:49:40 +0530

    Fixed showing coverage amount for hospital plan

--- src/app/shared/widgets/plan-widget/plan-widget.component.ts
+++ src/app/shared/widgets/plan-widget/plan-widget.component.ts
@@ -111,7 +111,7 @@ export class PlanWidgetComponent implements DoCheck, OnInit, AfterViewChecked {
       this.highlights.push({ title: 'Premium Duration:', description: this.premiumDuration });
 
       // For Whole Life
-      if (this.data.premium.durationName.indexOf('Whole Life') > -1) {
+      if (this.data.premium.durationName.indexOf('Whole Life') > -1 && this.type !== 'hospital plan') {
         this.highlights.push({ title: 'Coverage:', description: this.currency.transform(this.data.premium.sumAssured, 'USD', 'symbol', '1.0-0') + ` + Variable Bonus.` });
       }
 
