0d3d143d91888c5eaaaed44590cdfd06e752aa5a
Jawahar G <jawaharg@hcl.com>
2019-05-02 20:48:25 +0530

    Removed console.log

--- src/app/shared/widgets/plan-widget/plan-widget.component.ts
+++ src/app/shared/widgets/plan-widget/plan-widget.component.ts
@@ -119,7 +119,6 @@ export class PlanWidgetComponent implements DoCheck, OnInit, AfterViewChecked {
       if (this.data.premium.durationName.indexOf('Whole life w/Multiplier') > -1) {
         const age = this.data.insurer.insurerName.indexOf('Tokio Marine') > -1 ? 65 : 70;
         const multiplierRef = this.data.premium.multiplierRef || 3;
-        console.log(multiplierRef);
         const sumAssured = this.currency.transform(this.round.transform(this.data.premium.sumAssured / multiplierRef), 'USD', 'symbol', '1.0-0');
         this.highlights.push({ title: 'Coverage:', description: this.currency.transform(this.data.premium.sumAssured, 'USD', 'symbol', '1.0-0') + ` till age ${age}.` });
         this.highlights.push({ title: `(After age ${age}: ${sumAssured} + Variable Bonus.)`, description: '' });
