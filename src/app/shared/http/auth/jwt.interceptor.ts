95898389f371d8fd30bffb462ce4c613a8f575ba
Jawahar G <jawaharg@hcl.com>
2019-05-27 14:46:27 +0530

    BFA-1399 - Will Writing: After session time out , not able to download the will document pdf BFA-1400 - Logout: Not able to logout on after long ideal time (download wills)

--- src/app/shared/http/auth/jwt.interceptor.ts
+++ src/app/shared/http/auth/jwt.interceptor.ts
@@ -96,8 +96,8 @@ export class JwtInterceptor implements HttpInterceptor {
             if (err instanceof HttpErrorResponse) {
                 if (err.status === 401 || err.status === 403) {
                     this.auth.clearSession();
-                    this.errorHandler.handleAuthError(err);
                     this.signUpService.logoutUser();
+                    this.errorHandler.handleAuthError(err);
                 } else
                 if (err.message.match('I/O error on PUT request')) {
                     this.errorHandler.handleSubscribeError(err);
