f705f4f92eab95fcae05c689d35283cc8e93bd11
MONEYOWL/weihao <weihao@moneyowl.com.sg>
2019-06-27 15:57:18 +0800

    Updating Sign-Up Service - Adding configuration for email verification url

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -17,6 +17,7 @@ export interface IConfig {
   investmentEngagementEnabled: boolean;
   investmentMyInfoEnabled: boolean;
   comprehensiveEnabled: boolean;
+  verifyEmailUrl: string;
   hospitalPlanData: HospitalPlan[];
   productCategory: IProductCategory[];
 }
