399dc298d303f9e2664f88f4c58e2ce72615c506
saidevi <Koganti.Devi@hcl.com>
2019-07-02 13:07:31 +0530

    MO2-1953:fixed in dob empty scenario

--- src/app/sign-up/edit-profile/edit-profile.component.ts
+++ src/app/sign-up/edit-profile/edit-profile.component.ts
@@ -272,7 +272,13 @@ export class EditProfileComponent implements OnInit, OnDestroy {
     this.navbarService.unsubscribeBackPress();
   }
   constructDate(dob) {
-    const dateArr = dob.split('/');
-    this.dobFormat = dateArr[1] + '/' + dateArr[0] + '/' + dateArr[2];
+    if (dob) {
+      const dateArr = dob.split('/');
+      if (dateArr.length === 3) {
+        this.dobFormat = dateArr[1] + '/' + dateArr[0] + '/' + dateArr[2];
+    }
+   } else {
+     this.dobFormat = '';
+   }
   }
 }
