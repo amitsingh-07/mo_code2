5ce3978c7e6b27ef0a43411220d55a4eb308328b
Jawahar G <jawaharg@hcl.com>
2019-07-09 16:33:59 +0530

    BFA-1586 - [Robo1Prod] Session Expired but Login drop down menu is still available and doesn't throw session expired message

--- src/app/sign-up/auth-guard.service.ts
+++ src/app/sign-up/auth-guard.service.ts
@@ -3,17 +3,20 @@ import { CanActivate, Router } from '@angular/router';
 
 import { AuthenticationService } from '../shared/http/auth/authentication.service';
 import { SIGN_UP_ROUTE_PATHS } from './sign-up.routes.constants';
+import { SignUpService } from './sign-up.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
   constructor(private route: Router,
-              private authService: AuthenticationService
+              private authService: AuthenticationService,
+              private signUpService: SignUpService
   ) {
   }
   canActivate(): boolean {
     if (!this.authService.isSignedUser()) {
+      this.signUpService.logoutUser();
       this.route.navigate([SIGN_UP_ROUTE_PATHS.LOGIN]);
       return false;
     }
