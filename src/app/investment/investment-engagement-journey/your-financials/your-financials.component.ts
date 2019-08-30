d4ceca858bb5a3742a620db89c88a086a66fe1c3
Saidevi <saidevikoganti@ntuclink.com.sg>
2019-08-30 09:38:55 +0530

    MO2MP-13:updated financial details.

--- src/app/investment/investment-engagement-journey/your-financials/your-financials.component.ts
+++ src/app/investment/investment-engagement-journey/your-financials/your-financials.component.ts
@@ -198,6 +198,7 @@ export class YourFinancialsComponent implements IPageComponent, OnInit {
 
   isFirstTimeUser() {
     if (typeof this.financialFormValue.firstTimeUser === 'undefined') {
+      this.financialFormValue.firstTimeUser = true;
       return true;
     }
     return false;
