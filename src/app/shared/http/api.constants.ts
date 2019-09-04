1aa1bd622331cbc90aa6e9a1c0f7982b54c30119
praveen NT <praveen.nt@ntuclink.com.sg>
2019-09-04 11:11:14 +0530

    api constants code merge changes

--- src/app/shared/http/api.constants.ts
+++ src/app/shared/http/api.constants.ts
@@ -1,103 +1,107 @@
 import { map } from 'rxjs/operators';
-export const INVESTMENT_API_BASE_URL = 'invest/investment-microservice/';
-const ARTICLE_API_BASE_URL = 'product/insurance-product';
-const ABOUT_US_API_BASE_URL = 'product/insurance-product';
-const ACCOUNT_API_BASE_URL = 'account/account-microservice/api/';
-const SUBSCRIPTION_API_BASE_URL = 'product/insurance-product';
-const WILL_WRITING_API_BASE_URL = 'wills/wills-microservice/';
-const NOTIFICATION_API_BASE_URL = 'notification/notify-microservice';
 const COMPREHENSIVE_API_BASE_URL = 'recommend/recomm-microservice/api/customer/comprehensive/';
 const FINANCE_API_BASE_URL = 'finance/finhealth/api/customer/comprehensive/';
-const INSURANCE_API_BASE_URL = 'insurance/insurance-needs-microservice/api/customer/comprehensive/';
 const COMPREHENSIVE_REPORT_API_BASE_URL = 'comp/comprehensive-microservice/api/';
+const INVESTMENT_API_BASE_URL = 'invest/investment-microservice/api';
+const INSURANCE_API_BASE_URL = 'insurance/insurance-needs-microservice/api';
+const INSURANCE_RECOMMEND_API_BASE_URL = 'recommend/recomm-microservice/api';
+const ARTICLE_API_BASE_URL = 'product/insurance-product/api/article';
+const ABOUT_US_API_BASE_URL = 'product/insurance-product/api';
+const ACCOUNT_API_BASE_URL = 'account/account-microservice/api';
+const SUBSCRIPTION_API_BASE_URL = 'product/insurance-product/api';
+const WILL_WRITING_API_BASE_URL = 'wills/wills-microservice/api/wills';
+const NOTIFICATION_API_BASE_URL = 'notification/notify-microservice/api/notifications';
 
 export let apiConstants = {
     endpoint: {
         authenticate: 'account/account-microservice/authenticate',
         login: 'login',
-        logout: 'account/account-microservice/api/logout',
-        getProfileList: 'account/account-microservice/api/getProfileTypeList',
-        getProtectionTypesList: 'insurance/insurance-needs-microservice/api/getProtectionTypesList',
-        getLongTermCareList: 'insurance/insurance-needs-microservice/api/getCareGiverList',
-        getHospitalPlanList: 'insurance/insurance-needs-microservice/api/getHospitalClassList',
-        getRiskAssessmentQuestions: 'investment-microservice/api/RiskAssessment',
-        getRecommendations: 'recommend/recomm-microservice/api/getRecommendations',
-        updateProductEnquiry: 'account/account-microservice/api/updateCustomerEnquiry',
+        logout: ACCOUNT_API_BASE_URL + '/logout',
+        getProfileList: ACCOUNT_API_BASE_URL + '/getProfileTypeList',
+        getProtectionTypesList: INSURANCE_API_BASE_URL + '/getProtectionTypesList',
+        getLongTermCareList: INSURANCE_API_BASE_URL + '/getCareGiverList',
+        getHospitalPlanList: INSURANCE_API_BASE_URL + '/getHospitalClassList',
+        getRiskAssessmentQuestions: 'investment-microservice/RiskAssessment',
+        getRecommendations: INSURANCE_RECOMMEND_API_BASE_URL + '/getRecommendations',
+        updateProductEnquiry: ACCOUNT_API_BASE_URL + '/updateCustomerEnquiry',
         getMyInfoValues: 'sginfo/myinfo-microservice/api/getMyInfo',
-        signUp: 'account/account-microservice/api/signupV2',
-        updateUserId: 'account/account-microservice/api/updatePersonalDetails?handleError=true',
-        verifyOTP: 'account/account-microservice/api/verifyOTP',
-        resendOTP: 'account/account-microservice/api/resendOTP',
-        verifyEmail: 'account/account-microservice/api/verifyEmail',
-        resetPassword: 'account/account-microservice/api/resetPassword',
-        forgotPassword: 'account/account-microservice/api/forgotPassword',
-        userProfileInfo: 'account/account-microservice/api/getCustomerProfileDetails?handleError=true',
-        editContactDeatails: 'account/account-microservice/api/updateAddress',
-        editPassword: 'account/account-microservice/api/editPassword',
-        editProfile: 'account/account-microservice/api/customer/customerProfile',
-        editEmployerAddress: 'account/account-microservice/api/updateEmployment',
-        emailValidityCheck: 'account/account-microservice/api/emailValidityCheck',
-        detailCustomerSummary: 'account/account-microservice/api/getDetailedCustomerSummary',
-        getCustomerInsuranceDetails: 'recommend/recomm-microservice/api/customer/getCustomerInsuranceDetails',
-        resendEmailVerification: 'account/account-microservice/api/resendEmailVerification',
-        editMobileNumber: 'account/account-microservice/api/update-mobileno',
-        sendWelcomeMail: 'account/account-microservice/api/sendWelcomeMail',
-        registerBundleEnquiry: 'account/account-microservice/api/registerBundleEnquiry',
-        enquiryByEmail: 'account/account-microservice/api/enquiryByEmail',
+        signUp: ACCOUNT_API_BASE_URL + '/signupV2',
+        updateUserId: ACCOUNT_API_BASE_URL + '/updatePersonalDetails?handleError=true',
+        verifyOTP: ACCOUNT_API_BASE_URL + '/verifyOTP',
+        resendOTP: ACCOUNT_API_BASE_URL + '/resendOTP',
+        verifyEmail: ACCOUNT_API_BASE_URL + '/verifyEmail',
+        resetPassword: ACCOUNT_API_BASE_URL + '/resetPassword',
+        forgotPassword: ACCOUNT_API_BASE_URL + '/forgotPassword',
+        userProfileInfo: ACCOUNT_API_BASE_URL + '/getCustomerProfileDetails?handleError=true',
+        editContactDeatails: ACCOUNT_API_BASE_URL + '/updateAddress',
+        editPassword: ACCOUNT_API_BASE_URL + '/editPassword',
+        editProfile: ACCOUNT_API_BASE_URL + '/customer/customerProfile',
+        editEmployerAddress: ACCOUNT_API_BASE_URL + '/updateEmployment',
+        emailValidityCheck: ACCOUNT_API_BASE_URL + '/emailValidityCheck',
+        detailCustomerSummary: ACCOUNT_API_BASE_URL + '/getDetailedCustomerSummary',
+        getCustomerInsuranceDetails: INSURANCE_RECOMMEND_API_BASE_URL + '/customer/getDetailedInsuranceSummary?handleError=true',
+        resendEmailVerification: ACCOUNT_API_BASE_URL + '/resendEmailVerification',
+        editMobileNumber: ACCOUNT_API_BASE_URL + '/update-mobileno',
+        registerBundleEnquiry: ACCOUNT_API_BASE_URL + '/registerBundleEnquiry',
+        enquiryByEmail: ACCOUNT_API_BASE_URL + '/enquiryByEmail',
+        getSrsPopStatus: ACCOUNT_API_BASE_URL + '/getSrsPopStatus',
+        setSrsPopStatus: ACCOUNT_API_BASE_URL + '/setSrsPopStatus',
+        sendWelcomeMail: ACCOUNT_API_BASE_URL + '/sendWelcomeMail',
         article: {
-            getRecentArticles: ARTICLE_API_BASE_URL + '/api/article/getTop8Articles',
-            getArticleCategory: ARTICLE_API_BASE_URL + '/api/article/getCountForAllTags',
-            getArticleCategoryList: ARTICLE_API_BASE_URL + '/api/article/getArticlesByTagId',
-            getArticleCategoryAllList: ARTICLE_API_BASE_URL + '/api/article/getAllArticles',
-            getArticle: ARTICLE_API_BASE_URL + '/api/article/getArticleById',
-            getRelatedArticle: ARTICLE_API_BASE_URL + '/api/article/getTop3ArticlesByTagId'
+            getRecentArticles: ARTICLE_API_BASE_URL + '/getTop8Articles',
+            getArticleCategory: ARTICLE_API_BASE_URL + '/getCountForAllTags',
+            getArticleCategoryList: ARTICLE_API_BASE_URL + '/getArticlesByTagId',
+            getArticleCategoryAllList: ARTICLE_API_BASE_URL + '/getAllArticles',
+            getArticle: ARTICLE_API_BASE_URL + '/getArticleById',
+            getRelatedArticle: ARTICLE_API_BASE_URL + '/getTop3ArticlesByTagId'
         },
         aboutus: {
-            getCustomerReviews: ABOUT_US_API_BASE_URL + '/api/review/getAllReviews',
-            sendContactUs: ACCOUNT_API_BASE_URL + 'contactus'
+            getCustomerReviews: ABOUT_US_API_BASE_URL + '/review/getAllReviews',
+            sendContactUs: ACCOUNT_API_BASE_URL + '/contactus'
         },
         subscription: {
-            base: SUBSCRIPTION_API_BASE_URL + '/api/mailinglist/subscribe'
+            base: SUBSCRIPTION_API_BASE_URL + '/mailinglist/subscribe'
         },
         portfolio: {
-            setInvestmentObjective: INVESTMENT_API_BASE_URL + 'api/CustomerInvestmentObjective',
-            getRiskAssessmentQuestions: INVESTMENT_API_BASE_URL + 'api/RiskAssessment',
-            updateRiskAssessment: INVESTMENT_API_BASE_URL + 'api/RiskAssessment',
-            getAllocationDetails: INVESTMENT_API_BASE_URL + 'portfolio/recommend'
+            setInvestmentObjective: INVESTMENT_API_BASE_URL + '/CustomerInvestmentObjective',
+            setOneTimeInvestmentObjective: INVESTMENT_API_BASE_URL + '/portfolio/awaitingTransactions',
+            getRiskAssessmentQuestions: INVESTMENT_API_BASE_URL + '/RiskAssessment',
+            updateRiskAssessment: INVESTMENT_API_BASE_URL + '/RiskAssessment',
+            getAllocationDetails: INVESTMENT_API_BASE_URL + '/portfolio/recommend'
         },
         investmentAccount: {
-            nationalityCountrylist: INVESTMENT_API_BASE_URL + 'groupedCountryList',
-            nationalitylist: INVESTMENT_API_BASE_URL + 'countrylist',
+            nationalityCountrylist: INVESTMENT_API_BASE_URL + '/groupedCountryList',
+            nationalitylist: INVESTMENT_API_BASE_URL + '/countrylist',
+            lndustrylist: INVESTMENT_API_BASE_URL + '/industrylist',
+            occupationlist: INVESTMENT_API_BASE_URL + '/occupationlist',
+            allDropdownlist: INVESTMENT_API_BASE_URL + '/optionListCollection',
+            updateInvestment: INVESTMENT_API_BASE_URL + '/UpdateCustomerInvestmentObjective',
+            createInvestmentAccount: INVESTMENT_API_BASE_URL + '/createIFastAccount?handleError=true',
+            verifyAML: ACCOUNT_API_BASE_URL + '/verifyAML?handleError=true',
+            getFundTransferDetails: INVESTMENT_API_BASE_URL + '/getIFastBankDetails',
+            buyPortfolio: INVESTMENT_API_BASE_URL + '/portfolio/buy?handleError=true',
+            deletePortfolio: INVESTMENT_API_BASE_URL + '/customer/portfolios',
+            monthlyInvestment: INVESTMENT_API_BASE_URL + '/customer/InvestmentObjective/monthlyInvestment?handleError=true',
+            sellPortfolio: INVESTMENT_API_BASE_URL + '/portfolio/sell?handleError=true',
+            saveNationality: INVESTMENT_API_BASE_URL + '/customer/setNationality',
+            investmentoverview: INVESTMENT_API_BASE_URL + '/portfolio/holdings?handleError=true',
+            porfolioDetails: INVESTMENT_API_BASE_URL + '/portfolios/detail',
             getAddressByPincode: 'https://gothere.sg/maps/geo?output=json&client=&sensor=false',
-            lndustrylist: INVESTMENT_API_BASE_URL + 'industrylist',
-            occupationlist: INVESTMENT_API_BASE_URL + 'occupationlist',
-            allDropdownlist: INVESTMENT_API_BASE_URL + 'optionListCollection',
-            uploadDocument: 'account/account-microservice/saveDocuments',
-            saveInvestmentAccount: 'account/account-microservice/api/saveCustomerDetails',
-            saveNationality: 'invest/investment-microservice/customer/setNationality',
-            updateInvestment: INVESTMENT_API_BASE_URL + 'api/UpdateCustomerInvestmentObjective',
-            createInvestmentAccount: INVESTMENT_API_BASE_URL + 'createIFastAccount?handleError=true',
-            verifyAML: ACCOUNT_API_BASE_URL + 'verifyAML?handleError=true',
-            getFundTransferDetails: INVESTMENT_API_BASE_URL + 'getIFastBankDetails',
-            buyPortfolio: INVESTMENT_API_BASE_URL + 'portfolio/buy?handleError=true',
-            deletePortfolio: INVESTMENT_API_BASE_URL + 'customer/portfolios',
-            monthlyInvestment: INVESTMENT_API_BASE_URL + 'customer/InvestmentObjective/monthlyInvestment?handleError=true',
-            sellPortfolio: INVESTMENT_API_BASE_URL + 'portfolio/sell?handleError=true',
-            investmentoverview: 'invest/investment-microservice/portfolio/holdings?handleError=true',
-            porfolioDetails: 'invest/investment-microservice/portfolios/detail'
+            uploadDocument: ACCOUNT_API_BASE_URL + '/saveDocuments',
+            saveInvestmentAccount: ACCOUNT_API_BASE_URL + '/saveCustomerDetails'
         },
         investment: {
-            getUserAddress: 'account/account-microservice/api/customer/address',
-            getUserBankList: 'account/account-microservice/api/customer/banks',
-            addNewBank: INVESTMENT_API_BASE_URL + '/api/customer/bank?handleError=true',
+            getUserAddress: ACCOUNT_API_BASE_URL + '/customer/address',
+            getUserBankList: ACCOUNT_API_BASE_URL + '/customer/banks',
+            addNewBank: INVESTMENT_API_BASE_URL + '/customer/bank?handleError=true',
             getTransactions: INVESTMENT_API_BASE_URL + '/customer/transactions',
             getStatement: INVESTMENT_API_BASE_URL + '/getStatements',
-            monthlyInvestmentInfo: INVESTMENT_API_BASE_URL + 'api/CustomerInvestmentObjective',
+            monthlyInvestmentInfo: INVESTMENT_API_BASE_URL + '/CustomerInvestmentObjective',
         },
         notification: {
-            getRecentNotifications: NOTIFICATION_API_BASE_URL + '/api/notifications/recent',
-            getAllNotifications: NOTIFICATION_API_BASE_URL + '/api/notifications/getNotification',
-            updateNotifications: NOTIFICATION_API_BASE_URL + '/api/notifications/updateNotification'
+            getRecentNotifications: NOTIFICATION_API_BASE_URL + '/recent',
+            getAllNotifications: NOTIFICATION_API_BASE_URL + '/getNotification',
+            updateNotifications: NOTIFICATION_API_BASE_URL + '/updateNotification'
         },
         willWriting: {
             verifyPromoCode: 'account/account-microservice/api/promocode/validatePromoCode',
@@ -108,9 +112,9 @@ export let apiConstants = {
         },
         comprehensive: {
             getComprehensiveSummary: COMPREHENSIVE_API_BASE_URL + 'getComprehensiveUserSummary',
-            addPersonalDetails: ACCOUNT_API_BASE_URL + 'customer/comprehensive/addPersonalDetails',
-            addDependents: ACCOUNT_API_BASE_URL + 'customer/comprehensive/saveDependents',
-            saveEndowmentPlan: ACCOUNT_API_BASE_URL + 'customer/comprehensive/saveChildEndowmentPlans',
+            addPersonalDetails: ACCOUNT_API_BASE_URL + '/customer/comprehensive/addPersonalDetails',
+            addDependents: ACCOUNT_API_BASE_URL + '/customer/comprehensive/saveDependents',
+            saveEndowmentPlan: ACCOUNT_API_BASE_URL + '/customer/comprehensive/saveChildEndowmentPlans',
             saveEarnings: FINANCE_API_BASE_URL + 'saveEarnings',
             getSpendings: COMPREHENSIVE_API_BASE_URL + 'getExpenses',
             saveSpendings: FINANCE_API_BASE_URL + 'saveExpenses',
@@ -120,13 +124,18 @@ export let apiConstants = {
             saveRetirementPlan: INSURANCE_API_BASE_URL + 'saveRetirementPlanning',
             saveAssets: FINANCE_API_BASE_URL + 'saveComprehensiveAssets',
             saveLiabilities: FINANCE_API_BASE_URL + 'saveComprehensiveLiabilities',
-            getPromoCode: ACCOUNT_API_BASE_URL + 'customer/comprehensive/requestComprehensivePromoCode?category=COMPRE',
-            validatePromoCode: ACCOUNT_API_BASE_URL + 'customer/comprehensive/validateComprehensivePromoCode',
+            getPromoCode: ACCOUNT_API_BASE_URL + '/customer/comprehensive/requestComprehensivePromoCode?category=COMPRE',
+            validatePromoCode: ACCOUNT_API_BASE_URL + '/customer/comprehensive/validateComprehensivePromoCode',
             downloadComprehensiveReport: COMPREHENSIVE_REPORT_API_BASE_URL + 'downloadReportPdf',
             saveStepIndicator: COMPREHENSIVE_API_BASE_URL + 'saveComprehensiveStepCompletion',
             generateComprehensiveReport: COMPREHENSIVE_API_BASE_URL + 'generateComprehensiveReport',
             createReportRequest: COMPREHENSIVE_REPORT_API_BASE_URL + 'createReportRequest',
             getReport: COMPREHENSIVE_REPORT_API_BASE_URL + 'getReport',
+            verifyPromoCode: ACCOUNT_API_BASE_URL + '/promocode/validatePromoCode',
+            createWill: WILL_WRITING_API_BASE_URL + '/createWillProfile?handleError=true',
+            getWill: WILL_WRITING_API_BASE_URL + '/getWillProfile',
+            updateWill: WILL_WRITING_API_BASE_URL + '/updateWillProfile?handleError=true',
+            downloadWill: WILL_WRITING_API_BASE_URL + '/downloadWillDocument'
         }
     }
 };
