dc100b8ed78333e76a43a016e4430d0667e11c86
Rajasekar S P <rajasekar-s@hcl.com>
2019-09-13 19:01:26 +0530

    MO2MP-434 Clearing populated data after engagement journey

--- src/app/investment/investment-common/confirm-portfolio/confirm-portfolio.component.ts
+++ src/app/investment/investment-common/confirm-portfolio/confirm-portfolio.component.ts
@@ -474,6 +474,7 @@ export class ConfirmPortfolioComponent implements OnInit {
                 );
                 this.router.navigate([INVESTMENT_ACCOUNT_ROUTE_PATHS.SETUP_PENDING]);
               }
+              this.clearJourneyData();
             }
           }
         },
@@ -498,7 +499,6 @@ export class ConfirmPortfolioComponent implements OnInit {
       id: this.portfolio.customerPortfolioId
     };
     this.manageInvestmentsService.setToastMessage(toastMessage);
-    this.clearJourneyData();
     this.router.navigate([INVESTMENT_COMMON_ROUTE_PATHS.FUND_INTRO]);
   }
 
