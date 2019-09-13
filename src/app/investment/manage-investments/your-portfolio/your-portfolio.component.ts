dfe79f41a799cea3c0b513a42973fa96639c2f57
Vivek PT <vivek.pt@hs.moneyowl.com.sg>
2019-09-13 11:36:06 +0530

    MO2MP-22 Delete Portfolio Condition added

--- src/app/investment/manage-investments/your-portfolio/your-portfolio.component.ts
+++ src/app/investment/manage-investments/your-portfolio/your-portfolio.component.ts
@@ -226,7 +226,9 @@ export class YourPortfolioComponent implements OnInit {
       break;
     }
     case 5: {
-      this.showDeletePortfolioModal(this.portfolio);
+      if (this.portfolio.entitlements.showDelete) {
+        this.showDeletePortfolioModal(this.portfolio);
+      }
       break;
     }
   }
