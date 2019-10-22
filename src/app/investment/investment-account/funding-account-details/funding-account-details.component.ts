e1c428eede0042ae977375b948384720af8098c4
Yaseen <dudekula.y@hcl.com>
2019-10-22 16:04:20 +0530

    SRS Funding account details Base UI Created.

--- src/app/investment/investment-account/funding-account-details/funding-account-details.component.ts
+++ src/app/investment/investment-account/funding-account-details/funding-account-details.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+
+import { FooterService } from '../../../shared/footer/footer.service';
+import { AuthenticationService } from '../../../shared/http/auth/authentication.service';
+import { ErrorModalComponent } from '../../../shared/modal/error-modal/error-modal.component';
+import { NavbarService } from '../../../shared/navbar/navbar.service';
+import {
+    AccountCreationErrorModalComponent
+} from '../../investment-common/confirm-portfolio/account-creation-error-modal/account-creation-error-modal.component';
+import { INVESTMENT_ACCOUNT_ROUTE_PATHS } from '../investment-account-routes.constants';
+import { InvestmentAccountService } from '../investment-account-service';
+import { INVESTMENT_ACCOUNT_CONSTANTS } from '../investment-account.constant';
 
 @Component({
   selector: 'app-funding-account-details',
   templateUrl: './funding-account-details.component.html',
-  styleUrls: ['./funding-account-details.component.scss']
+  styleUrls: ['./funding-account-details.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FundingAccountDetailsComponent implements OnInit {
-
-  constructor() { }
+  pageTitle: string;
+  sourceOfIncomeList=['Enter SRS Operator','Cash','SRS'];
+  constructor(
+    public navbarService: NavbarService,
+    public footerService: FooterService,
+    public activeModal: NgbActiveModal,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private investmentAccountService: InvestmentAccountService,
+    private modal: NgbModal,
+    public authService: AuthenticationService,
+    public readonly translate: TranslateService
+  ) {
+    this.translate.use('en');
+    this.translate.get('COMMON').subscribe((result: string) => {
+      this.pageTitle = this.translate.instant('fdhdgfgh');
+      this.setPageTitle(this.pageTitle);
+    });
+  }
+  setPageTitle(title: string) {
+    this.navbarService.setPageTitle(title);
+  }
 
   ngOnInit() {
+  }
+  selectSource(value){
+    console.log(value);
+
   }
 
 }
