28af32fa19a6fb38456f2da2d7ba588acabda75e
Jawahar G <jawaharg@ntuclink.com.sg>
2019-11-07 19:26:11 +0530

    BFA-1746 - Update retirement plan step1

--- src/app/retirement-planning/retirement-plan-step1/retirement-plan-step1.component.ts
+++ src/app/retirement-planning/retirement-plan-step1/retirement-plan-step1.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NgbDateParserFormatter, NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbDateCustomParserFormatter } from 'src/app/shared/utils/ngb-date-custom-parser-formatter';
 
 import { NavbarService } from 'src/app/shared/navbar/navbar.service';
-import { RegexConstants } from 'src/app/shared/utils/api.regex.constants';
+import { RetirementPlanningService } from '../retirement-planning.service';
+
+import { RETIREMENT_PLANNING_ROUTE_PATHS } from '../retirement-planning-routes.constants';
 
 @Component({
   selector: 'app-retirement-plan-step1',
   templateUrl: './retirement-plan-step1.component.html',
   styleUrls: ['./retirement-plan-step1.component.scss'],
+  providers: [
+    { provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter }
+  ],
   encapsulation: ViewEncapsulation.None
 })
 export class RetirementPlanStep1Component implements OnInit {
@@ -18,6 +25,7 @@ export class RetirementPlanStep1Component implements OnInit {
   submitted: boolean = false;
 
   retirementNeedsForm: FormGroup;
+  formValues: any;
 
   constructor(
     public navbarService: NavbarService,
@@ -25,6 +33,8 @@ export class RetirementPlanStep1Component implements OnInit {
     private parserFormatter: NgbDateParserFormatter,
     private config: NgbDatepickerConfig,
     private formBuilder: FormBuilder,
+    private router: Router,
+    public retirementPlanningService: RetirementPlanningService
   ) {
     this.translate.use('en');
     this.translate.get('COMMON').subscribe((result: string) => {
@@ -48,45 +58,61 @@ export class RetirementPlanStep1Component implements OnInit {
   ngOnInit() {
     this.navbarService.setNavbarMode(6);
     this.navbarService.setNavbarShadowVisibility(false);
-
+    this.formValues = this.retirementPlanningService.getRetirementNeeds();
     this.buildFormData();
   }
 
   buildFormData() {
     this.retirementNeedsForm = this.formBuilder.group({
-      retirementAge: ['', [Validators.required]],
-      retirementIncome: ['', [Validators.required]],
-      customerDob: ['', [Validators.required]],
-      lumpsumAmount: [''],
-      monthlyAmount: ['']
+      retirementAge: [this.formValues.retirementNeeds && this.formValues.retirementNeeds.retirementAge, [Validators.required]],
+      monthlyRetirementIncome: [this.formValues.retirementNeeds && this.formValues.retirementNeeds.monthlyRetirementIncome, [Validators.required]],
+      dateOfBirth: [this.formValues.retirementNeeds && this.formValues.retirementNeeds.dateOfBirth, [Validators.required]],
+      lumpSumAmount: [this.formValues.retirementAmountAvailable && this.formValues.retirementAmountAvailable.lumpSumAmount],
+      monthlyAmount: [this.formValues.retirementAmountAvailable && this.formValues.retirementAmountAvailable.monthlyAmount]
     }, { validator: this.checkAge() });
   }
 
 
   checkAge() {
     return (group: FormGroup) => {
-      const customerDob = group.controls['customerDob'];
+      const dateOfBirth = group.controls['dateOfBirth'];
       const retirementAge = group.controls['retirementAge'];
-      const dob = customerDob.value;
-      const today = new Date();
-      const birthDate = new Date(dob['year'] + '/' + dob['month'] + '/' + dob['day']);
-      let age = today.getFullYear() - birthDate.getFullYear();
-      const m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-      }
 
-      if (age && retirementAge.value < age) {
-        customerDob.setErrors({invalidAge: true});
-        console.log(age);
-      } else if(age && retirementAge.value > age) {
-        customerDob.setErrors(null);
+      if (dateOfBirth.value) {
+        const dob = dateOfBirth.value;
+        const today = new Date();
+        const birthDate = new Date(dob['year'] + '/' + dob['month'] + '/' + dob['day']);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+        }
+
+        if (age && retirementAge.value < age) {
+          dateOfBirth.setErrors({ invalidAge: true });
+        } else if (age && retirementAge.value > age) {
+          dateOfBirth.setErrors(null);
+        }
       }
     };
   }
 
-  save(formData) {
+  save(form) {
     this.submitted = true;
-    console.log(formData);
+    if (form.valid) {
+      const retirementNeedsGroup = {
+        retirementNeeds: {
+          retirementAge: form.value.retirementAge,
+          monthlyRetirementIncome: form.value.monthlyRetirementIncome,
+          dateOfBirth: form.value.dateOfBirth
+        },
+        retirementAmountAvailable: {
+          lumpSumAmount: form.value.lumpSumAmount,
+          monthlyAmount: form.value.monthlyAmount
+        }
+      };
+      this.retirementPlanningService.setRetirementNeeds(retirementNeedsGroup);
+      this.router.navigate([RETIREMENT_PLANNING_ROUTE_PATHS.STEP_2]);
+    }
   }
 }
