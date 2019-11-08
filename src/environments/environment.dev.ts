2d4811dc8e77652101e5e053d9b514faae3664e4
Sakthi M <sakthimuthusamy@ntuclink.com.sg>
2019-11-08 12:48:09 +0530

    Resetting the DEV and UAT environments path.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -8,9 +8,9 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: false,
   isDebugMode: true,
-  apiBaseUrl: 'https://bfa-dev2.ntucbfa.cloud',
+  apiBaseUrl: 'https://bfa-dev.ntucbfa.cloud',
   myInfoClientId: 'STG2-MYINFO-SELF-TEST',
-  myInfoCallbackBaseUrl: 'https://bfa-dev2.ntucbfa.cloud/myinfo',
+  myInfoCallbackBaseUrl: 'https://bfa-dev.ntucbfa.cloud/myinfo',
   myInfoAuthorizeUrl: 'https://test.api.myinfo.gov.sg/com/v3/authorise',
   gAdPropertyId: 'AW-782920447',
   gaPropertyId: 'UA-125256446-1'
