65ae16f6dd2103ed495e07d5bac550ee5bdad65e
NeoKianGuan <kianguan@moneyowl.com.sg>
2019-11-13 18:12:20 +0800

    SEO: Fix for Google Crawler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -96,7 +96,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
       this.footerService.setFooterVisibility(true);
       // Meta Tag and Title Methods
       this.seoService.setTitle('Insurance Comparison | Term Insurance | Invest & Wills | MoneyOwl');
-      this.seoService.setBaseSocialMetaTags(this.translate.instant('GENERAL.META.META_TITLE'),
+      this.seoService.setBaseSocialMetaTags('Insurance Comparison | Term Insurance | Invest & Wills | MoneyOwl',
         this.translate.instant('GENERAL.META.META_DESCRIPTION'),
         this.translate.instant('GENERAL.META.META_KEYWORDS'));
       this.meta.addTag({ name: 'copyright', content: this.translate.instant('GENERAL.META.META_COPYRIGHT') });
