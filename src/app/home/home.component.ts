8b9ab23d6df4b7436789c79214a9dace66559aa0
NeoKianGuan <kianguan@moneyowl.com.sg>
2019-11-14 11:50:54 +0800

    SEO TESTING

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -95,8 +95,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
       this.navbarService.setNavbarShadowVisibility(true);
       this.footerService.setFooterVisibility(true);
       // Meta Tag and Title Methods
-      this.seoService.setTitle('Insurance Comparison | Term Insurance | Invest & Wills | MoneyOwl');
-      this.seoService.setBaseSocialMetaTags(this.translate.instant('GENERAL.META.META_TITLE'),
+      this.seoService.setTitle('title here');
+      this.seoService.setBaseSocialMetaTags('meta tag title here',
         this.translate.instant('GENERAL.META.META_DESCRIPTION'),
         this.translate.instant('GENERAL.META.META_KEYWORDS'));
       this.meta.addTag({ name: 'copyright', content: this.translate.instant('GENERAL.META.META_COPYRIGHT') });
