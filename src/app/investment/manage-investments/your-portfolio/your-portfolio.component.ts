e291eae128b7d673e9912fab21ec19c4f74d7580
Rajasekar S P <rajasekar-s@hcl.com>
2019-12-04 17:28:12 +0530

    MO2MP-1048 - Rebalancing cash withdrawal issue fixed.

--- src/app/investment/manage-investments/your-portfolio/your-portfolio.component.ts
+++ src/app/investment/manage-investments/your-portfolio/your-portfolio.component.ts
@@ -18,7 +18,7 @@ import {
 import { ProfileIcons } from '../../investment-engagement-journey/recommendation/profileIcons';
 import { IToastMessage } from '../manage-investments-form-data';
 import { MANAGE_INVESTMENTS_ROUTE_PATHS } from '../manage-investments-routes.constants';
-import { MANAGE_INVESTMENTS_CONSTANTS } from '../manage-investments.constants';
+import { MANAGE_INVESTMENTS_CONSTANTS, PORTFOLIO_WITHDRAWAL_KEYS } from '../manage-investments.constants';
 import { ManageInvestmentsService } from '../manage-investments.service';
 import { SignUpService } from './../../../sign-up/sign-up.service';
 import { RenameInvestmentModalComponent } from './rename-investment-modal/rename-investment-modal.component';
@@ -46,6 +46,7 @@ export class YourPortfolioComponent implements OnInit {
   showErrorMessage: boolean;
   toastMsg: any;
   activeTab: string;
+  portfolioWithdrawRequests: boolean = false;
 
   constructor(
     public readonly translate: TranslateService,
@@ -112,12 +113,21 @@ export class YourPortfolioComponent implements OnInit {
           this.pendingOnetimeBuyRequests = this.groupBuyRequests(this.pendingBuyRequests, 'ONE_TIME');
           this.pendingMonthlyBuyRequests = this.groupBuyRequests(this.pendingBuyRequests, 'MONTHLY');
         }
+        if (this.pendingSellRequests && this.pendingSellRequests.value) {
+          this.portfolioWithdrawRequests = this.getPortfolioWithdrawalRequests(this.pendingSellRequests.value);
+        }
       }
       this.showOrHideWhatsNextSection();
     },
       (err) => {
         this.investmentAccountService.showGenericErrorModal();
       });
+  }
+
+  getPortfolioWithdrawalRequests(sellRequests) {
+    return sellRequests.filter(
+      (req) => PORTFOLIO_WITHDRAWAL_KEYS.indexOf(req.paymentMode) >= 0
+    );
   }
 
   groupBuyRequests(buyRequests, transactionFrequency) {
@@ -194,7 +204,7 @@ export class YourPortfolioComponent implements OnInit {
   gotoTopUp(monthly?: boolean) {
     const data = this.manageInvestmentsService.getTopUp();
     data['Investment'] = monthly ?
-    MANAGE_INVESTMENTS_CONSTANTS.TOPUP.MONTHLY_INVESTMENT : MANAGE_INVESTMENTS_CONSTANTS.TOPUP.ONETINE_INVESTMENT;
+      MANAGE_INVESTMENTS_CONSTANTS.TOPUP.MONTHLY_INVESTMENT : MANAGE_INVESTMENTS_CONSTANTS.TOPUP.ONETINE_INVESTMENT;
     this.manageInvestmentsService.setTopUp(data);
     this.manageInvestmentsService.setSelectedCustomerPortfolioId(this.portfolio.customerPortfolioId);
     this.router.navigate([MANAGE_INVESTMENTS_ROUTE_PATHS.TOPUP]);
@@ -309,7 +319,7 @@ export class YourPortfolioComponent implements OnInit {
       if (response.responseMessage.responseCode >= 6000) {
         this.showToastMessage(this.portfolio.portfolioName, portfolioName);
         this.getCustomerPortfolioDetailsById(this.portfolio.customerPortfolioId);
-        this.manageInvestmentsService.updateNewPortfolioName(this.portfolio.customerPortfolioId, portfolioName);
+        this.manageInvestmentsService.updateNewPortfolioName(this.portfolio.customerPortfolioId, portfolioName);
         this.showErrorMessage = false;
       } else if (response.responseMessage.responseCode === 5120) {
         this.showErrorMessage = true;
