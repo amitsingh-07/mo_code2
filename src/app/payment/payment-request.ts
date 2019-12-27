9bfe0fae7dde81a9c6f1ec09b1f3c6ef1cdc85fd
NeoKianGuan <kianguan@moneyowl.com.sg>
2019-12-27 14:45:00 +0800

    Update redirect url

--- src/app/payment/payment-request.ts
+++ src/app/payment/payment-request.ts
@@ -8,7 +8,7 @@ export const PaymentRequest: IPaymentRequest = {
     transactionType: 'purchase',
     currency: 'SGD',
     ipAddress: '127.0.0.1',
-    redirectURL: 'https://bfa-dev.ntucbfa.cloud/api/redirectPaymentStatus',
+    redirectURL: 'https://bfa-dev.ntucbfa.cloud/payment/api/redirectPaymentStatus',
     secretKey: '03365d5f-1a12-4f16-9351-7ee59ddc9d3f',
     requestURL: 'https://test.wirecard.com.sg/engine/hpp/'
 };
