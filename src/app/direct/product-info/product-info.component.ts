fd4b242c798f81d59a71b8588582a88ade35d179
Jawahar G <jawaharg@hcl.com>
2020-02-24 17:10:58 +0530

    BFA-1890 - update dob and gender for mobile

--- src/app/direct/product-info/product-info.component.ts
+++ src/app/direct/product-info/product-info.component.ts
@@ -322,7 +322,9 @@ export class ProductInfoComponent implements OnInit, OnDestroy {
     this.selectedCategory = data;
     this.selectedCategoryId = data.id;
     this.setActiveProductCategory(index);
-    this.directService.updateUserInfo();
+    if (this.innerWidth < this.mobileThreshold) {
+      this.directService.updateUserInfo();
+    }
   }
 
   setActiveProductCategory(index) {
