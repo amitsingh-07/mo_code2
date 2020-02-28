53467e70d1b4e0dec1415f721e19b2c1b320740a
NeoKianGuan <kianguan@moneyowl.com.sg>
2020-02-28 16:14:15 +0800

    Hide about us and all legal links in footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+import { environment } from './../../../environments/environment';
 import { ConfigService, IConfig } from './../../config/config.service';
 import { FooterService } from './footer.service';
 
@@ -13,6 +14,8 @@ export class FooterComponent implements OnInit, AfterViewInit {
   showFooter = false;
   isMaintenanceEnabled = false;
   copyrightYear: string;
+  showFooterItems = true;
+
   constructor(
     private footerService: FooterService, private cdr: ChangeDetectorRef,
     public readonly translate: TranslateService, private configService: ConfigService) {
@@ -20,6 +23,9 @@ export class FooterComponent implements OnInit, AfterViewInit {
       this.translate.use(config.language);
       this.isMaintenanceEnabled = config.maintenanceEnabled;
     });
+    if (environment.hideHomepage) {
+      this.showFooterItems = false;
+    }
   }
 
   ngOnInit() {
