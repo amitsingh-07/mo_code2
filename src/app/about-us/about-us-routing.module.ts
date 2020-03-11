38dfec064f4fa67184d2bc6a4fccfdf3bbe8514c
NeoKianGuan <kianguan@moneyowl.com.sg>
2020-03-11 12:06:37 +0800

    Hide routing for about us, customer-reviews, why-moneyowl

--- src/app/about-us/about-us-routing.module.ts
+++ src/app/about-us/about-us-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { environment } from './../../environments/environment';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CustomerReviewsComponent } from './customer-reviews/customer-reviews.component';
 import { WhyMoneyOwlComponent } from './why-money-owl/why-money-owl.component';
 
-const routes: Routes = [
+let routes: Routes = [
   { path: '', component: AboutUsComponent},
   { path: 'customer-reviews', component: CustomerReviewsComponent},
   { path: 'contact-us', component: ContactUsComponent},
   { path: 'why-moneyowl', component: WhyMoneyOwlComponent},
   { path: '**', redirectTo: '/page-not-found' }
 ];
+
+if (environment.hideHomepage) {
+  routes = [
+    { path: 'contact-us', component: ContactUsComponent},
+    { path: '', redirectTo: '/page-not-found' },
+    { path: '**', redirectTo: '/page-not-found' }
+  ];
+}
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
