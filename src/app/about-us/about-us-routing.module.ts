30d066232e2dfc1a598cebf1e63acb2ed1f3def2
NeoKianGuan <kianguan@moneyowl.com.sg>
2020-03-17 17:32:30 +0800

    Add about-us guard to prevent navigation

--- src/app/about-us/about-us-routing.module.ts
+++ src/app/about-us/about-us-routing.module.ts
@@ -1,27 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { environment } from './../../environments/environment';
+import { AboutUsEnableGuard } from './about-us-enable-guard';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CustomerReviewsComponent } from './customer-reviews/customer-reviews.component';
 import { WhyMoneyOwlComponent } from './why-money-owl/why-money-owl.component';
 
-let routes: Routes = [
-  { path: '', component: AboutUsComponent},
-  { path: 'customer-reviews', component: CustomerReviewsComponent},
+const routes: Routes = [
+  { path: '', component: AboutUsComponent, canActivate: [AboutUsEnableGuard] },
+  { path: 'customer-reviews', component: CustomerReviewsComponent, canActivate: [AboutUsEnableGuard] },
   { path: 'contact-us', component: ContactUsComponent},
-  { path: 'why-moneyowl', component: WhyMoneyOwlComponent},
+  { path: 'why-moneyowl', component: WhyMoneyOwlComponent, canActivate: [AboutUsEnableGuard] },
   { path: '**', redirectTo: '/page-not-found' }
 ];
-
-if (environment.hideHomepage) {
-  routes = [
-    { path: 'contact-us', component: ContactUsComponent},
-    { path: '', redirectTo: '/page-not-found' },
-    { path: '**', redirectTo: '/page-not-found' }
-  ];
-}
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
