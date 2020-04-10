e62c23bac74d9fb099518fa94d254bc83e4e9666
Jawahar G <jawaharg@hcl.com>
2020-04-10 12:29:09 +0530

    BFA-2117 - Added condition for hideHomepage

--- src/app/email-enquiry-success/email-enquiry-success.component.ts
+++ src/app/email-enquiry-success/email-enquiry-success.component.ts
@@ -1,11 +1,13 @@
 import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { APP_ROUTES } from './../app-routes.constants';
 import { appConstants } from '../app.constants';
 import { AuthenticationService } from './../shared/http/auth/authentication.service';
 import { SIGN_UP_ROUTE_PATHS } from './../sign-up/sign-up.routes.constants';
 
 import { FooterService } from '../shared/footer/footer.service';
 import { NavbarService } from '../shared/navbar/navbar.service';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-email-enquiry-success',
@@ -16,10 +18,11 @@ import { NavbarService } from '../shared/navbar/navbar.service';
 export class EmailEnquirySuccessComponent implements OnInit {
 
   isSignedUser: boolean;
+  routeName: string;
 
   @HostListener('window:popstate', ['$event'])
   onPopState(event) {
-    this.router.navigate([appConstants.homePageUrl]);
+    this.router.navigate([this.routeName]);
   }
 
   constructor(
@@ -28,6 +31,11 @@ export class EmailEnquirySuccessComponent implements OnInit {
     public authService: AuthenticationService,
     private router: Router) {
     this.isSignedUser = this.authService.isSignedUser();
+    if (environment.hideHomepage) {
+      this.routeName = appConstants.homePageUrl;
+    } else {
+      this.routeName = APP_ROUTES.HOME;
+    }
   }
 
   ngOnInit() {
@@ -37,7 +45,7 @@ export class EmailEnquirySuccessComponent implements OnInit {
 
   redirectTo() {
     if (!this.isSignedUser) {
-      this.router.navigate([appConstants.homePageUrl]);
+      this.router.navigate([this.routeName]);
     } else {
       this.router.navigate([SIGN_UP_ROUTE_PATHS.DASHBOARD]);
     }
