65c9fdf072a8f41feb6a14fd23f445825cbaf1fe
NeoKianGuan <kianguan@moneyowl.com.sg>
2020-07-20 17:44:10 +0800

    Account-status unit test update

--- src/app/investment/investment-account/account-status/account-status.component.spec.ts
+++ src/app/investment/investment-account/account-status/account-status.component.spec.ts
@@ -1,66 +1,26 @@
-import { Location } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { Injector } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, fakeAsync, getTestBed, inject, TestBed, tick } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HttpModule, XHRBackend } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-import { By } from '@angular/platform-browser';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { Router, Routes } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-import { JwtModule } from '@auth0/angular-jwt';
-import { NgbActiveModal, NgbModal, NgbModalRef, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateService } from '@ngx-translate/core';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { concat, Observable, of, throwError } from 'rxjs';
-import 'rxjs/add/observable/of';
+import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
+import { RouterTestingModule} from '@angular/router/testing';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
 
-import { CurrencyPipe } from '@angular/common';
-import { appConstants } from '../../../app.constants';
-
-
-import { tokenGetterFn } from 
-'../../../../assets/mocks/service/shared-service';
-
-import { FooterService } from '../../../shared/footer/footer.service';
-import { HeaderService } from '../../../shared/header/header.service';
-import { NavbarService } from '../../../shared/navbar/navbar.service';
-import { InvestmentApiService } from '../../investment-api.service';
-
-import { AppService } from './../../../app.service';
-import { LoaderService } from './../../../shared/components/loader/loader.service';
-import { ApiService } from './../../../shared/http/api.service';
-import { AuthenticationService } from './../../../shared/http/auth/authentication.service';
-
-import { InvestmentTitleBarComponent } from '../../../shared/components/investment-title-bar/investment-title-bar.component';
-import { ErrorModalComponent } from '../../../shared/modal/error-modal/error-modal.component';
-
+import { tokenGetterFn } from 'src/app/app.module';
 import { AccountStatusComponent } from './account-status.component';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
-
-
-import { SIGN_UP_ROUTE_PATHS } from '../../../sign-up/sign-up.routes.constants';
-import {
-    INVESTMENT_COMMON_ROUTE_PATHS
-} from '../../investment-common/investment-common-routes.constants';
-import { InvestmentCommonService } from '../../investment-common/investment-common.service';
-import {
-    MANAGE_INVESTMENTS_ROUTE_PATHS
-} from '../../manage-investments/manage-investments-routes.constants';
-import { ManageInvestmentsService } from '../../manage-investments/manage-investments.service';
-import { InvestmentAccountService } from '../investment-account-service';
-import { INVESTMENT_ACCOUNT_CONSTANTS } from '../investment-account.constant';
 
 describe('AccountStatusComponent', () => {
   let component: AccountStatusComponent;
   let fixture: ComponentFixture<AccountStatusComponent>;
+  let routerSpy = {navigate: jasmine.createSpy('navigate')};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AccountStatusComponent]
+      imports: [TranslateModule.forRoot(), RouterTestingModule.withRoutes([]), NgbModule.forRoot(), HttpClientTestingModule, HttpModule,
+         JwtModule.forRoot({config: {}})],
+      declarations: [AccountStatusComponent],
+      providers: [NgbActiveModal, JwtHelperService, { provide: Router, useValue: routerSpy }]
     })
       .compileComponents();
   }));
@@ -71,7 +31,12 @@ describe('AccountStatusComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create AccountStatusComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to Dashboard', () => {
+    component.redirectToDashboard();
+    expect(routerSpy).toHaveBeenCalledWith(['../accounts/dashboard']);
   });
 });
