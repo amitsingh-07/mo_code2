d66b43afcfc49b9ae341ca0ec3493c7f029b9e15
NeoKianGuan <kianguan@moneyowl.com.sg>
2020-12-22 16:08:03 +0800

    MO2MP-2023: Remove promo code section on 1st portfolio creation journey

--- src/app/investment/investment-engagement-journey/start-journey/start-journey.component.ts
+++ src/app/investment/investment-engagement-journey/start-journey/start-journey.component.ts
@@ -1,10 +1,8 @@
 import { Location } from '@angular/common';
 import {
-    Component, ElementRef, HostListener, OnInit, ViewChild, ViewEncapsulation
+    Component, OnInit, ViewEncapsulation
 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
 
 import { appConstants } from '../../../app.constants';
@@ -13,11 +11,8 @@ import { LoaderService } from '../../../shared/components/loader/loader.service'
 import { FooterService } from '../../../shared/footer/footer.service';
 import { HeaderService } from '../../../shared/header/header.service';
 import { AuthenticationService } from '../../../shared/http/auth/authentication.service';
-import { ErrorModalComponent } from '../../../shared/modal/error-modal/error-modal.component';
 import { NavbarService } from '../../../shared/navbar/navbar.service';
-import { RegexConstants } from '../../../shared/utils/api.regex.constants';
 import { INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS } from '../investment-engagement-journey-routes.constants';
-import { InvestmentEngagementJourneyService } from '../investment-engagement-journey.service';
 import { SeoServiceService } from './../../../shared/Services/seo-service.service';
 
 @Component({
@@ -29,30 +24,24 @@ import { SeoServiceService } from './../../../shared/Services/seo-service.servic
 export class StartJourneyComponent implements OnInit {
   pageTitle: string;
   isDisabled: boolean;
-  promoCode;
   errorMsg: string;
-  promoCodeForm: FormGroup;
-  @ViewChild('promoCode') promoCodeRef: ElementRef;
+
   constructor(
     public readonly translate: TranslateService,
     private router: Router,
     private loaderService: LoaderService,
     private appService: AppService,
     public headerService: HeaderService,
-    private investmentEngagementJourneyService: InvestmentEngagementJourneyService,
     public navbarService: NavbarService,
     public footerService: FooterService,
     public authService: AuthenticationService,
     private _location: Location,
-    private formBuilder: FormBuilder,
-    private modal: NgbModal,
     private seoService: SeoServiceService
   ) {
     this.translate.use('en');
     this.translate.get('COMMON').subscribe((result: string) => {
       this.pageTitle = this.translate.instant('START.PAGE_TITLE');
       this.setPageTitle(this.pageTitle);
-      this.errorMsg = this.translate.instant('START.PROMO_ERROR');
       // Meta Tag and Title Methods
       this.seoService.setTitle(this.translate.instant('START.META.META_TITLE'));
       this.seoService.setBaseSocialMetaTags(this.translate.instant('START.META.META_TITLE'),
@@ -68,14 +57,6 @@ export class StartJourneyComponent implements OnInit {
 
     this.authService.authenticate().subscribe((token) => {
     });
-    this.promoCodeForm = this.formBuilder.group({
-      promoCode: ['', [Validators.pattern(RegexConstants.SixDigitPromo)]]
-    });
-  }
-
-  @HostListener('input', ['$event'])
-  onChange() {
-    this.promoCodeRef.nativeElement.value = this.promoCodeRef.nativeElement.value.toUpperCase();
   }
 
   setPageTitle(title: string) {
@@ -86,53 +67,10 @@ export class StartJourneyComponent implements OnInit {
   }
   goNext() {
     this.appService.setJourneyType(appConstants.JOURNEY_TYPE_INVESTMENT);
-    if (this.promoCodeForm.controls.promoCode.value) {
-      this.verifyPromoCode(this.promoCodeForm.controls.promoCode.value);
-    } else {
-      this.authService.saveEnquiryId(null);
-      this.redirectToNextScreen();
-    }
+    this.authService.saveEnquiryId(null);
+    this.redirectToNextScreen();
   }
-
-  verifyPromoCode(promoCode) {
-    this.loaderService.showLoader({
-      title: this.translate.instant(
-        'COMMON_LOADER.TITLE'
-      ),
-      desc: this.translate.instant(
-        'COMMON_LOADER.DESC'
-      )
-    });
-    promoCode = promoCode.toUpperCase();
-    this.isDisabled = true;
-    this.investmentEngagementJourneyService.verifyPromoCode(promoCode).subscribe((data) => {
-      this.loaderService.hideLoader();
-      this.promoCode = data.responseMessage;
-      if (this.promoCode.responseCode === 6005) {
-        this.authService.saveEnquiryId(data.objectList[0].enquiryId);
-       this.redirectToNextScreen();
-      } else if (this.promoCode.responseCode === 5017) {
-        this.showErrorModal();
-        this.isDisabled = false;
-      } else {
-        this.isDisabled = false;
-        return false;
-      }
-    }, (error) => {
-      this.loaderService.hideLoader();
-      this.isDisabled = false;
-    });
-  }
-  
   redirectToNextScreen(){
     this.router.navigate([INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS.SELECT_PORTFOLIO]);
-  }
-  
-
-  showErrorModal() {
-    const ref = this.modal.open(ErrorModalComponent, { centered: true });
-    ref.componentInstance.errorTitle = 'Error';
-    ref.componentInstance.errorDescription = this.errorMsg;
-    return false;
   }
 }
