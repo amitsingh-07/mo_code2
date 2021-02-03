f7d7678d6168e4f71b1d69d30e072685a6e7aab9
Saidevi <koganti.devi@hcl.com>
2021-02-04 00:23:08 +0530

    commit the MO2MP-2776 And MO2MP-2940.

--- src/app/investment/manage-investments/fees/fees.component.ts
+++ src/app/investment/manage-investments/fees/fees.component.ts
@@ -2,23 +2,16 @@ import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
-
-import { appConstants } from '../../../app.constants';
 import { AppService } from '../../../app.service';
 import { FooterService } from '../../../shared/footer/footer.service';
 import { HeaderService } from '../../../shared/header/header.service';
-import { AuthenticationService } from '../../../shared/http/auth/authentication.service';
-import {
-  ModelWithButtonComponent
-} from '../../../shared/modal/model-with-button/model-with-button.component';
+import { AuthenticationService } from '../../../shared/http/auth/authentication.service'
+
 import { NavbarService } from '../../../shared/navbar/navbar.service';
-import { SIGN_UP_ROUTE_PATHS } from '../../../sign-up/sign-up.routes.constants';
 import { SignUpService } from '../../../sign-up/sign-up.service';
-import {
-  INVESTMENT_ACCOUNT_ROUTE_PATHS
-} from '../../investment-account/investment-account-routes.constants';
+
 import { InvestmentEngagementJourneyService } from '../../investment-engagement-journey/investment-engagement-journey.service';
-import { RiskProfile } from '../../investment-engagement-journey/recommendation/riskprofile';
+
 import { ManageInvestmentsService } from '../manage-investments.service';
 import { environment } from './../../../../environments/environment';
 @Component({
@@ -46,11 +39,11 @@ export class FeesComponent implements OnInit {
     public manageInvestmentsService: ManageInvestmentsService,
     private signUpService: SignUpService,
     private investmentEngagementJourneyService: InvestmentEngagementJourneyService
-  ) {       
+  ) {
     this.translate.use('en');
     const self = this;
     this.translate.get('COMMON').subscribe((result: string) => {
-      self.pageTitle = this.translate.instant('Fees');
+      self.pageTitle = this.translate.instant('FEES.TITLE');
       this.setPageTitle(this.pageTitle);
       this.renderer.addClass(document.body, 'portfolioname-bg');
     });
@@ -70,22 +63,12 @@ export class FeesComponent implements OnInit {
   setPageTitle(title: string) {
     this.navbarService.setPageTitle(title);
   }
-  getFeeDetails(){
+  getFeeDetails() {
     this.manageInvestmentsService.getFeeDetails().subscribe((data) => {
       this.feeDetails = data.objectList;
-      console.log(this.feeDetails +'Fee Details')
     });
   }
-  goToNext() {
-    console.log('test1');
-  }
-  showHide(el) {
-    if (el.style.display === '' || el.style.display === 'block') {
-      el.style.display = 'none';
-    } else {
-      el.style.display = 'block';
-    }
-  }
+
   ngOnDestroy() {
     this.renderer.removeClass(document.body, 'portfolioname-bg');
   }
