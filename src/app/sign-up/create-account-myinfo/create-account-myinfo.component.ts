ad004a6d26c0ccfbcb841e656f5a78de8bc88933
Saidevi <koganti.devi@hcl.com>
2021-03-24 16:29:39 +0530

    commit the console error.

--- src/app/sign-up/create-account-myinfo/create-account-myinfo.component.ts
+++ src/app/sign-up/create-account-myinfo/create-account-myinfo.component.ts
@@ -94,8 +94,6 @@ export class CreateAccountMyinfoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.createAccountData = this.signUpService.getUserProfileInfo();
-    (this.createAccountData && this.createAccountData.isMyInfoEnabled) ? false : true;
     this.myinfoChangeListener = this.myInfoService.changeListener.subscribe((myinfoObj: any) => {
       if (myinfoObj && myinfoObj !== '' &&
         this.myInfoService.getMyInfoAttributes() === this.signUpService.myInfoAttributes.join()) {
