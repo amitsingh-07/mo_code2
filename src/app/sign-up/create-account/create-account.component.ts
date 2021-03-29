8e0bcc94eb39315c5c46fbdae96ccfb05bd4d661
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-03-29 14:30:18 +0530

    MO2MP-2760

--- src/app/sign-up/create-account/create-account.component.ts
+++ src/app/sign-up/create-account/create-account.component.ts
@@ -122,7 +122,7 @@ export class CreateAccountComponent implements OnInit, AfterViewInit {
     this.buildAccountInfoForm();
     this.getCountryCode();
     //Referral Code snapshot param
-    if (this.route.snapshot.paramMap.get('referralCode') !== '' && this.createAccountForm.controls['referralCode']) {      
+    if (this.route.snapshot.paramMap.get('referralCode') != '' && this.createAccountForm.controls['referralCode']) {      
       this.createAccountForm.controls['referralCode'].setValue(this.route.snapshot.paramMap.get('referralCode'));
       this.showClearBtn = true;
     }
