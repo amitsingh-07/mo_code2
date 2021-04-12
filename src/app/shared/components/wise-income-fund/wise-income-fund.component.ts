19cd8c841170ff280c89a323188eaa26838e587b
yaseen dudekula <dudekulayaseen@hs.moneyowl.com.sg>
2021-04-12 10:47:36 +0530

    MO2MP-3633 : Initial review inputs journey

--- src/app/shared/components/wise-income-fund/wise-income-fund.component.ts
+++ src/app/shared/components/wise-income-fund/wise-income-fund.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
 
 import {
   INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS
 } from './../../../investment/investment-engagement-journey/investment-engagement-journey-routes.constants';
 import { InvestmentAccountService } from '../../../investment/investment-account/investment-account-service';
 import { InvestmentCommonService } from '../../../investment/investment-common/investment-common.service';
-
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { EditInvestmentModalComponent } from '../../modal/edit-investment-modal/edit-investment-modal.component';
+import { AuthenticationService } from '../../http/auth/authentication.service';
+import {
+  IInvestmentCriteria
+} from '../../../investment/investment-common/investment-common-form-data';
 @Component({
   selector: 'app-wise-income-fund',
   templateUrl: './wise-income-fund.component.html',
@@ -14,14 +19,18 @@ import { InvestmentCommonService } from '../../../investment/investment-common/i
   encapsulation: ViewEncapsulation.None
 })
 export class WiseIncomeFundComponent implements OnInit {
-
+  @Input() investmentData: any;
+  // @Input() investmentCriteria: IInvestmentCriteria;
   @Input('portfolio') portfolio;
   @Input('investmentInput') investmentInput;
-  
+  investmentCriteria: IInvestmentCriteria;
+
 
   constructor(private router: Router,
     private investmentAccountService: InvestmentAccountService,
-    private investmentCommonService: InvestmentCommonService) { }
+    private investmentCommonService: InvestmentCommonService,
+    public modal: NgbModal,
+    public authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
@@ -29,7 +38,56 @@ export class WiseIncomeFundComponent implements OnInit {
   goReviewInputs() {
     this.investmentAccountService.activateReassess();
     this.investmentCommonService.saveUpdateSessionData(this.portfolio);
-    this.router.navigate([INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS.INVESTMENT_AMOUNT]);
+    // this.router.navigate([INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS.INVESTMENT_AMOUNT]);
+    this.getInvestmentCriteria(this.portfolio);
+    this.openEditInvestmentModal();
   }
-
+  openEditInvestmentModal() {
+    const ref = this.modal.open(EditInvestmentModalComponent, {
+      centered: true
+    });
+    ref.componentInstance.investmentData = {
+      oneTimeInvestment: this.portfolio.initialInvestment,
+      monthlyInvestment: this.portfolio.monthlyInvestment
+    };
+    ref.componentInstance.investmentCriteria = this.investmentCriteria;
+    ref.componentInstance.modifiedInvestmentData.subscribe((emittedValue) => {
+      // update form data
+      ref.close();
+      this.saveUpdatedInvestmentData(emittedValue);
+    });
+    this.dismissPopup(ref);
+  }
+  dismissPopup(ref: NgbModalRef) {
+    this.router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        ref.close();
+      }
+    });
+  }
+  saveUpdatedInvestmentData(updatedData) {
+    const params = this.constructUpdateInvestmentParams(updatedData);
+    const customerPortfolioId = this.portfolio.customerPortfolioId;
+    this.investmentAccountService.updateInvestment(customerPortfolioId, params).subscribe((data) => {
+      this.router.navigate([INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS.PORTFOLIO_RECOMMENDATION]);
+    },
+      (err) => {
+        this.investmentAccountService.showGenericErrorModal();
+      });
+  }
+  constructUpdateInvestmentParams(data) {
+    return {
+      initialInvestment: parseFloat(data.oneTimeInvestment),
+      monthlyInvestment: parseFloat(data.monthlyInvestment),
+      enquiryId: this.authService.getEnquiryId()
+    };
+  }
+  getInvestmentCriteria(portfolioValues) {
+    if (portfolioValues.portfolioType) {
+      this.investmentCommonService.getInvestmentCriteria(portfolioValues.portfolioType).subscribe((data) => {
+        this.investmentCriteria = data;
+      });
+    }
+  }
 }
+
