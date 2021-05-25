5759377ee06f709d28b7838d4a905209ccf60905
yaseen dudekula <dudekulayaseen@hs.moneyowl.com.sg>
2021-05-25 15:40:45 +0530

    MO2MP-3873 : Mobile UI commit

--- src/app/sign-up/refer-a-friend/refer-a-friend.component.ts
+++ src/app/sign-up/refer-a-friend/refer-a-friend.component.ts
@@ -3,6 +3,7 @@ import {
   SIGN_UP_ROUTE_PATHS
 } from '../../sign-up/sign-up.routes.constants';
 import { Router } from '@angular/router';
+import { NavbarService } from '../../shared/navbar/navbar.service';
 @Component({
   selector: 'app-refer-a-friend',
   templateUrl: './refer-a-friend.component.html',
@@ -10,15 +11,32 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None
 })
 export class ReferAFriendComponent implements OnInit {
-  isCollapsed = false;
-  constructor(private router: Router,) {
-    
+  isCollapsed: boolean = false;
+  pageTitle: string;
+  constructor(
+    private router: Router,
+    public navbarService: NavbarService,
+    ) {
+    this.pageTitle = "Refer a friend";
+    this.setPageTitle(this.pageTitle);
    }
 
   ngOnInit(): void {
+    this.navbarService.setNavbarMobileVisibility(true);
+    this.navbarService.setNavbarMode(6);
   }
  
+
+  setPageTitle(title: string) {
+    this.navbarService.setPageTitle(title);
+  }
+
+
   goToRewards(){
     this.router.navigate([SIGN_UP_ROUTE_PATHS.DASHBOARD]);
+  }
+
+  toggleinfo(event) {
+    this.isCollapsed = !this.isCollapsed;
   }
 }
