7d306b88d3a46e5215ad0565e54030f5670666e9
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-06-10 17:19:15 +0530

    Removed unwanted code

--- src/app/sign-up/verify-mobile/verify-mobile.component.ts
+++ src/app/sign-up/verify-mobile/verify-mobile.component.ts
@@ -479,7 +479,6 @@ export class VerifyMobileComponent implements OnInit, OnDestroy {
     this.authService.doValidate2faLogin(otp, userEmail, journeyType, enqId  ).subscribe((data: any) => {
       if (data.responseMessage.responseCode  >= 6000) {
         this.mobileNumberVerified = true;
-        //this.authService.set2FAToken(data.responseMessage.responseCode);
         this.mobileNumberVerifiedMessage = this.loading['verified2fa'];
         
         this.progressModal = false;
