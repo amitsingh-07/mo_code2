8aa9e56b7160243a2091c9b36c4bdd4e89d8e636
yaseen dudekula <dudekulayaseen@hs.moneyowl.com.sg>
2021-07-08 20:36:39 +0530

    MO3-2531 : DOB UI changes

--- src/app/promo-code/ntuc-member/ntuc-member.component.ts
+++ src/app/promo-code/ntuc-member/ntuc-member.component.ts
@@ -5,11 +5,13 @@ import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { RegexConstants } from '../../shared/utils/api.regex.constants';
 import { PromoCodeService } from '../promo-code.service';
-
+import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateCustomParserFormatter } from '../../shared/utils/ngb-date-custom-parser-formatter';
 @Component({
   selector: 'app-ntuc-member',
   templateUrl: './ntuc-member.component.html',
   styleUrls: ['./ntuc-member.component.scss'],
+  providers: [{ provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter }],
   encapsulation: ViewEncapsulation.None
 })
 export class NtucMemberComponent implements OnInit {
@@ -19,13 +21,14 @@ export class NtucMemberComponent implements OnInit {
  
   constructor(
     public activeModal: NgbActiveModal,
+    private parserFormatter: NgbDateParserFormatter,
     private promoSvc: PromoCodeService) {
   }
   ngOnInit() {    
     this.promo = this.promoSvc.getSelectedPromo();
     this.ntucMumberForm = new FormGroup({
       mobileNumber: new FormControl(this.promo.mobileNo),
-      dob: new FormControl(this.promo.dateOfBirth),
+      dob: new FormControl(this.parserFormatter.parse(this.promo.dateOfBirth)),
       nricOrFin: new FormControl('', [Validators.required,Validators.pattern(RegexConstants.nricOrFinLastFourCharacters)])
     });
   }
