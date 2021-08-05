802d92a0632c813a55c7bd0123a6b340aba129bc
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-08-05 18:26:02 +0530

    MO3-2758

--- src/app/comprehensive/dashboard/dashboard.component.ts
+++ src/app/comprehensive/dashboard/dashboard.component.ts
@@ -139,7 +139,7 @@ constructor(
 
   
   goToEditProfile() {
-    if (this.comprehensivePlanning === 4 && !this.versionTypeEnabled && !this.isCFPGetStarted) {
+    if (this.comprehensivePlanning === 4 && !this.isCFPGetStarted) {
       this.router.navigate([COMPREHENSIVE_ROUTE_PATHS.ROOT]);
     } else {
       this.setComprehensiveSummary(true, COMPREHENSIVE_ROUTE_PATHS.GETTING_STARTED);
@@ -171,7 +171,7 @@ constructor(
             };
             this.comprehensiveService.openTooltipModalWithDismiss(toolTipParams);
           } else {
-            this.comprehensiveApiService.getComprehensiveSummary(this.getCurrentVersionType).subscribe((data: any) => {
+            this.comprehensiveApiService.getComprehensiveSummary().subscribe((data: any) => {
               if (data && data.objectList[0]) {
                   this.comprehensiveService.setComprehensiveSummary(data.objectList[0]);
                   this.router.navigate([COMPREHENSIVE_ROUTE_PATHS.GETTING_STARTED]);
@@ -181,7 +181,7 @@ constructor(
       (this.reportStatus === COMPREHENSIVE_CONST.REPORT_STATUS.READY) || (this.reportStatus === COMPREHENSIVE_CONST.REPORT_STATUS.ERROR)){
         this.getComprehensiveCall();
     } else{
-      this.comprehensiveApiService.getComprehensiveSummary(this.getCurrentVersionType).subscribe((data: any) => {
+      this.comprehensiveApiService.getComprehensiveSummary().subscribe((data: any) => {
         if (data && data.objectList[0]) {
             this.comprehensiveService.setComprehensiveSummary(data.objectList[0]);
             this.router.navigate([COMPREHENSIVE_ROUTE_PATHS.GETTING_STARTED]);
@@ -199,7 +199,7 @@ constructor(
     const payload = {enquiryId: this.enquiryId, reportStatus : reportStatusValue};
     this.comprehensiveApiService.updateComprehensiveReportStatus(payload).subscribe((data: any) => {
           if (data) {
-            this.comprehensiveApiService.getComprehensiveSummary(this.getCurrentVersionType).subscribe((summaryData: any) => {
+            this.comprehensiveApiService.getComprehensiveSummary().subscribe((summaryData: any) => {
               if (summaryData) {
                 summaryData.objectList[0].comprehensiveEnquiry.reportStatus = COMPREHENSIVE_CONST.REPORT_STATUS.EDIT;
                 this.comprehensiveService.setComprehensiveSummary(summaryData.objectList[0]);
@@ -251,7 +251,7 @@ constructor(
       this.isLoadComplete = false;
     }
     this.comprehensivePlanning = 4;
-    this.comprehensiveApiService.getComprehensiveSummary(this.getCurrentVersionType).subscribe((summaryData: any) => {
+    this.comprehensiveApiService.getComprehensiveSummary().subscribe((summaryData: any) => {
       if (summaryData && summaryData.objectList[0]) {
         this.comprehensiveService.setComprehensiveSummary(summaryData.objectList[0]);
         this.userDetails = this.comprehensiveService.getMyProfile();
