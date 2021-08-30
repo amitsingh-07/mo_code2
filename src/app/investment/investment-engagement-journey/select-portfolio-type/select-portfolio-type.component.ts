17d3d791809dbbcedd452fa0ad4998c3c21b8067
HCLTECH\khateejathul.kubraa <khateejathul.kubraa@hcl.com>
2021-08-30 23:27:12 +0530

    added redirection from portfolio type

--- src/app/investment/investment-engagement-journey/select-portfolio-type/select-portfolio-type.component.ts
+++ src/app/investment/investment-engagement-journey/select-portfolio-type/select-portfolio-type.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { INVESTMENT_COMMON_ROUTE_PATHS } from '../../investment-common/investment-common-routes.constants';
+import { INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS } from '../investment-engagement-journey-routes.constants';
 
 @Component({
   selector: 'app-select-portfolio-type',
@@ -8,9 +11,16 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SelectPortfolioTypeComponent implements OnInit {
 
-  constructor() { }
+  isPersonalAccount: boolean;
+  portfolioType: any;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    console.log("account type", this.portfolioType);
   }
 
+  goToNext() {
+    this.portfolioType === 1 ? this.isPersonalAccount = true : this.isPersonalAccount = false;
+    this.isPersonalAccount ? this.router.navigate(['../' + INVESTMENT_COMMON_ROUTE_PATHS.ADD_SECONDARY_HOLDER_DETAILS]) : this.router.navigate([INVESTMENT_ENGAGEMENT_JOURNEY_ROUTE_PATHS.SELECT_PORTFOLIO]);
+  }
 }
