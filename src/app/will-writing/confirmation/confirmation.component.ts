c90addb2ed9d9def112b954773400474e369843f
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-09-15 15:39:59 +0530

    BFA-3158:assigned default value as Y for isEngSpeaker payload and removed the unused constant isNotEngSpeaker

--- src/app/will-writing/confirmation/confirmation.component.ts
+++ src/app/will-writing/confirmation/confirmation.component.ts
@@ -32,7 +32,6 @@ export class ConfirmationComponent implements OnInit, OnDestroy {
   createWillTriggered = false;
 
   abtMeInfo: IAboutMe;
-  isNotEngSpeaker: boolean = false;
 
   constructor(
     private translate: TranslateService,
@@ -58,9 +57,6 @@ export class ConfirmationComponent implements OnInit, OnDestroy {
     this.willWritingService.setFromConfirmPage(false);
     this.willWritingFormData = this.willWritingService.getWillWritingFormData();
     this.abtMeInfo = this.willWritingFormData.aboutMe;
-    if (this.abtMeInfo['isEngSpeaker'] === false) {
-      this.isNotEngSpeaker = true;
-    }
     const estateDistribution = this.willWritingFormData.beneficiary.filter((beneficiary) => beneficiary.selected === true);
     this.willBeneficiary = estateDistribution;
     for (const beneficiary of estateDistribution) {
