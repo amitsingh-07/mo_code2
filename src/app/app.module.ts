2e92318ee1d93250cb0fd021c2f185202d875a96
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-11-01 12:35:33 +0530

    change PWA config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -210,7 +210,7 @@ export function tokenGetterFn() {
         tokenGetter: tokenGetterFn
       }
     }),
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/app/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {
