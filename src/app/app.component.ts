f6cbc9fcda3fbcf88e190b03dde1dd450b900a02
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-11-22 12:59:35 +0530

    code changes for add home screen button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -206,31 +206,31 @@ export class AppComponent implements IComponentCanDeactivate, OnInit, AfterViewI
     this.sessionsService.setActiveInstance(instId);
    }
     //A2HS
-  deferredPrompt: any;
-  showButton = false;
-  @HostListener('window:beforeinstallprompt', ['$event'])
-  onbeforeinstallprompt(e) {
-    console.log(e);
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    this.deferredPrompt = e;
-    this.showButton = true;
-  }
-  addToHomeScreen() {
-    // hide our user interface that shows our A2HS button
-    this.showButton = false;
-    // Show the prompt
-    this.deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    this.deferredPrompt.userChoice
-    .then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    this.deferredPrompt = null;
-  });
-  }
+  let deferredPrompt;
+  const addBtn = document.querySelector('.home-add-button');
+  addBtn.style.display = 'none';
+  window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+	  e.preventDefault();
+	  // Stash the event so it can be triggered later.
+	  deferredPrompt = e;
+	  // Update UI to notify the user they can add to home screen
+	  addBtn.style.display = 'block';
+
+	  addBtn.addEventListener('click', (e) => {
+		// hide our user interface that shows our A2HS button
+		addBtn.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+			  console.log('User accepted the A2HS prompt');
+			} else {
+			  console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		  });
+	  });
+	});
 }
