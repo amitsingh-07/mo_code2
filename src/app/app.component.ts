1d530fe2dfa3b31fb21f15a7b281c2dc442ace1e
esakkimuthum <esakkimuthu@hs.moneyowl.com.sg>
2021-11-25 12:46:03 +0530

    ios fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -233,4 +233,15 @@ export class AppComponent implements IComponentCanDeactivate, OnInit, AfterViewI
     this.deferredPrompt = null;
   });
   }
+  const isIos = () => {
+	  const userAgent = window.navigator.userAgent.toLowerCase();
+	  return /iphone|ipad|ipod/.test( userAgent );
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+	// Checks if should display install popup notification:
+	if (isIos() && !isInStandaloneMode()) {
+	  this.setState({ showInstallMessage: true });
+	}
 }
