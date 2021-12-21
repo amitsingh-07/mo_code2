39ef6dd1131e36a7884200f7fb468a9fb10fc599
Ajay Maharana <ajayakumarm@moneyowl.sg>
2021-12-21 12:31:57 +0530

    MO2MP-5061 and MO2MP-5057: FE Development

---
+++ src/app/investment/investment-engagement-journey/cka-passed-result/cka-passed-result.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { FooterService } from '../../../shared/footer/footer.service';
+import { HeaderService } from '../../../shared/header/header.service';
+import { NavbarService } from '../../../shared/navbar/navbar.service';
+
+@Component({
+  selector: 'app-cka-passed-result',
+  templateUrl: './cka-passed-result.component.html',
+  styleUrls: ['./cka-passed-result.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CkaPassedResultComponent implements OnInit {
+  pageTitle: string;
+
+  constructor(
+    public readonly translate: TranslateService,
+    private router: Router,
+    private _location: Location,
+    public navbarService: NavbarService,
+    public headerService: HeaderService,
+    public footerService: FooterService
+  ) {
+    this.translate.use('en');
+   }
+
+  ngOnInit(): void {
+    this.navbarService.setNavbarMode(6);
+    this.navbarService.setNavbarMobileVisibility(false);
+    this.footerService.setFooterVisibility(false);
+  }
+  goBack() {
+    this._location.back();
+  }
+}
