8d51f2a58b643016a14180bff83cbbe99528edfc
Haider Ali N <haiderali.n@hcl.com>
2022-04-06 14:56:05 +0530

    FB - redirecting to corp login page after updating password and mobile nr through profile

--- src/app/sign-up/auth-guard.service.ts
+++ src/app/sign-up/auth-guard.service.ts
@@ -97,7 +97,6 @@ export class FacebookLoggedUserService implements CanActivate {
       this.route.navigate([SIGN_UP_ROUTE_PATHS.DASHBOARD]);
       return false;
     }
-    this.authService.isUserTypeCorporate = true;
     return true;
   }
 }
