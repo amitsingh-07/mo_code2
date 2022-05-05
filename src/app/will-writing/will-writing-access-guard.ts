9be1bc17851eb7f33ac152f3a4980d0dafd01753
HCLTECH\khateejathul.kubraa <khateejathul.kubraa@hcl.com>
2022-05-05 17:24:39 +0530

    refresh issue fixes

--- src/app/will-writing/will-writing-access-guard.ts
+++ src/app/will-writing/will-writing-access-guard.ts
@@ -3,6 +3,7 @@ import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppService } from '../app.service';
+import { AuthenticationService } from '../shared/http/auth/authentication.service';
 import { SIGN_UP_ROUTE_PATHS } from '../sign-up/sign-up.routes.constants';
 import { WILL_WRITING_ROUTE_PATHS } from './will-writing-routes.constants';
 import { WillWritingService } from './will-writing.service';
@@ -12,17 +13,37 @@ export class WillWritingAccessGuard implements CanActivate {
   constructor(
     private willWritingService: WillWritingService,
     private router: Router,
-    private appService: AppService
+    private appService: AppService,
+    private authService: AuthenticationService
   ) {
   }
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const promoCode = this.willWritingService.getPromoCode();
-    if (Object.keys(promoCode).length === 0 && promoCode.constructor === Object && !this.willWritingService.getIsWillCreated()) {
+    if (!this.authService.isSignedUser() && this.appService.getCorporateDetails().organisationEnabled) {
+      this.router.navigate([SIGN_UP_ROUTE_PATHS.CORPORATE_LOGIN], { queryParams: { orgID: this.appService.getCorporateDetails().uuid } });
+    } else if (Object.keys(promoCode).length === 0 && promoCode.constructor === Object && !this.willWritingService.getIsWillCreated()) {
       if (this.appService.getCorporateDetails().organisationEnabled) {
-        this.router.navigate([SIGN_UP_ROUTE_PATHS.CORPORATE_LOGIN], { queryParams: {orgID: this.appService.getCorporateDetails().uuid}});
+        this.router.navigate([SIGN_UP_ROUTE_PATHS.CORPORATE_LOGIN], { queryParams: { orgID: this.appService.getCorporateDetails().uuid } });
       } else {
         this.router.navigate([WILL_WRITING_ROUTE_PATHS.INTRODUCTION]);
       }
+      return false;
+    }
+    return true;
+  }
+}
+
+@Injectable()
+export class WillWritingIntroductionGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private appService: AppService,
+    private authService: AuthenticationService
+  ) {
+  }
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.isSignedUser() && this.appService.getCorporateDetails().organisationEnabled) {
+      this.router.navigate([SIGN_UP_ROUTE_PATHS.CORPORATE_LOGIN], { queryParams: { orgID: this.appService.getCorporateDetails().uuid } });
       return false;
     }
     return true;
