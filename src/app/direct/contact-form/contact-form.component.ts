395c3034e123e7af03b337fb31cc5e1a1dc5b0d3
Yaseen D <dudekulayaseen@hs.moneyowl.com.sg>
2022-05-19 16:57:05 +0530

    BFA-3430 : Added error msg conditions in html and ts

--- src/app/direct/contact-form/contact-form.component.ts
+++ src/app/direct/contact-form/contact-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { SIGN_UP_CONFIG } from 'src/app/sign-up/sign-up.constant';
 
 @Component({
   selector: 'app-contact-form',
@@ -11,8 +12,21 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class ContactFormComponent implements OnInit {
   formObject: FormGroup;
   isSubmitted = false;
+  formValue: any;
+  maxDate: any;
+  minDate: any;
 
-  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder) {}
+  constructor(public activeModal: NgbActiveModal, private fb: FormBuilder) {
+    const today: Date = new Date();
+    this.minDate = {
+      year: today.getFullYear() - SIGN_UP_CONFIG.ACCOUNT_CREATION.DOB.DATE_PICKER_MAX_YEAR,
+      month: today.getMonth() + 1, day: today.getDate()
+    };
+    this.maxDate = {
+      year: today.getFullYear() - SIGN_UP_CONFIG.ACCOUNT_CREATION.DOB.DATE_PICKER_MIN_YEAR,
+      month: today.getMonth() + 1, day: today.getDate()
+    };
+  }
 
   ngOnInit(): void {
     this.buildForm();
@@ -28,6 +42,7 @@ export class ContactFormComponent implements OnInit {
       interedtedInsurance: ['', Validators.required],
       isSmoker: ['', Validators.required],
       pepOtherOccupation: ['', Validators.required],
+      enquiry : ['']
     })
   }
 
