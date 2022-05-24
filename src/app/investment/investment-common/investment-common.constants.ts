fb7a5ac2a379a7e9fc69fe90d070b4aecbe066d8
vinaya <vinaya@hs.moneyowl.com.sg>
2022-05-24 09:47:00 +0530

    added roundoff amount for below 0.04 in const file

--- src/app/investment/investment-common/investment-common.constants.ts
+++ src/app/investment/investment-common/investment-common.constants.ts
@@ -154,5 +154,8 @@ export let INVESTMENT_COMMON_CONSTANTS = {
     EQUITIES: 'Equities',
     FIXED_INCOME: 'Fixed Income',
     BONDS: 'Bonds'
+  },
+  ASSET_ALLOCATION_CONST: {
+    MIN_AMOUNT_FOR_ROUNDOFF: 0.05
   }
 };
