113576434049c012b408111c359a9644759fb8d9
preethas <preethas@hs.moneyowl.com.sg>
2022-06-08 13:19:04 +0530

    Addressed PR comments

--- src/app/sign-up/corp-biz-activation-link/corp-biz-activation-link.component.ts
+++ src/app/sign-up/corp-biz-activation-link/corp-biz-activation-link.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FooterService } from '../../shared/footer/footer.service';
 import { NavbarService } from '../../shared/navbar/navbar.service';
 import { AuthenticationService } from '../../shared/http/auth/authentication.service';
@@ -16,14 +16,13 @@ import { SIGN_UP_CONFIG } from '../sign-up.constant';
 })
 export class CorpBizActivationLinkComponent implements OnInit {
   screenToShow: string;
-  queryParams;
-  token;
+  queryParams: Params;
+  token: string;
   
   constructor(private router: Router,public footerService: FooterService, 
     public navbarService: NavbarService,private translate: TranslateService, private route: ActivatedRoute,
     private authService: AuthenticationService,private signUpApiService: SignUpApiService) { 
       this.translate.use('en');
-      this.screenToShow = SIGN_UP_CONFIG.CORP_BIZ_ACTIVATIONLINK.INVALID_USER; // temp screen
     }
 
   ngOnInit(): void {
