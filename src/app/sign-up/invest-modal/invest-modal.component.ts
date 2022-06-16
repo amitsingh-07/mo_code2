cf03d339eff97f788a4bb024720b3850f454974e
preethas <preethas@hs.moneyowl.com.sg>
2022-06-16 15:16:43 +0530

    MO2MP-5925

---
+++ src/app/sign-up/invest-modal/invest-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invest-modal',
+  templateUrl: './invest-modal.component.html',
+  styleUrls: ['./invest-modal.component.scss']
+})
+export class InvestModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
