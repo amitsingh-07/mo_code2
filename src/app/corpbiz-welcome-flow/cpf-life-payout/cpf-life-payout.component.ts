d61b120d7cd8123ab9fc5a4817d8f8b6ad2896d0
Haider Ali N <haiderali.n@hcl.com>
2022-06-17 11:53:44 +0530

    MO3-3274 - Report generate api done with mock api response

--- src/app/corpbiz-welcome-flow/cpf-life-payout/cpf-life-payout.component.ts
+++ src/app/corpbiz-welcome-flow/cpf-life-payout/cpf-life-payout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ComprehensiveService } from '../../comprehensive/comprehensive.service';
 import { FooterService } from '../../shared/footer/footer.service';
 import { NavbarService } from '../../shared/navbar/navbar.service';
 
@@ -10,23 +11,19 @@ import { NavbarService } from '../../shared/navbar/navbar.service';
   encapsulation: ViewEncapsulation.None
 })
 export class CpfLifePayoutComponent implements OnInit {
-  showCpfLifePayout: boolean;
-  cpfPayoutAmount: string = "$4,000";
+  showCpfLifePayout: boolean = false;
 
   constructor( 
     private footerService: FooterService,
     private navbarService: NavbarService,
-    private translate: TranslateService) {
+    private translate: TranslateService,
+    public comprehensiveService: ComprehensiveService) {
       this.translate.use('en');
      }
 
   ngOnInit(): void {
     this.navbarService.setNavbarMode(101);
     this.footerService.setFooterVisibility(false);
-    this.showCpfLifePayout = false;
-  }
-  showPayout(){
-    this.showCpfLifePayout = true;
   }
 
 }
