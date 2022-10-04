245d1c963bd3d5ffb4378010285e141a1eb161cd
NeoKianGuan <kianguan@moneyowl.com.sg>
2022-10-04 15:35:20 +0800

    Refactor code and fix for iphone

--- src/app/shared/utils/file.util.ts
+++ src/app/shared/utils/file.util.ts
@@ -1,43 +1,49 @@
 import { Injectable } from '@angular/core';
 export const FILE_TYPE = 'application/pdf';
+const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
 @Injectable()
 export class FileUtil {
 
   public downloadPDF(data: any, fileName: string) {
     let newWindow;
-    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
     if (iOS) {
       newWindow = window.open();
-    }
-    const pdfUrl = window.URL.createObjectURL(data);
-      if (iOS) {
-        if (newWindow.document.readyState === 'complete') {
-          newWindow.location.assign(pdfUrl);
-        } else {
-          newWindow.onload = () => {
-            newWindow.location.assign(pdfUrl);
-          };
-        }
+      const pdfUrl = window.URL.createObjectURL(data);
+      if (newWindow.document.readyState === 'complete') {
+        newWindow.location.assign(pdfUrl);
       } else {
-        this.saveAs(data, fileName);
+        newWindow.onload = () => {
+          newWindow.location.assign(pdfUrl);
+        };
       }
+    } else {
+      this.saveAs(data, fileName);
+    }
   }
 
   private saveAs(data: any, fileName: string): void {
     const blob = new Blob([data], { type: FILE_TYPE });
+    const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+    const otherBrowsers = /Android|Windows|Mac/.test(navigator.userAgent);
+
     if (window.navigator && window.navigator.msSaveOrOpenBlob) {
       window.navigator.msSaveOrOpenBlob(blob, fileName);
-    } else {
+    } else if ((isSafari && iOS) || otherBrowsers || isSafari) {
       setTimeout(() => {
-        this.downloadFile(data, fileName);
+        this.downloadFile(data, fileName, blob);
       }, 1000);
+    } else {
+      const reader: any = new FileReader();
+      reader.onload = ((e) => {
+        window.open(reader.result);
+      });
+      reader.readAsDataURL(blob);
     }
   }
 
-  private downloadFile(data: any, fileName: string): void {
-    const blob = new Blob([data], { type: FILE_TYPE });
-    const url = window.URL.createObjectURL(blob);
+  private downloadFile(data: any, fileName: string, blobFile: Blob): void {
+    const url = window.URL.createObjectURL(blobFile);
     const a = document.createElement('a');
     document.body.appendChild(a);
     a.setAttribute('style', 'display: none');
