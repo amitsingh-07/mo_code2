e07274cabdba98bf19d853829c9746668fc71d42
NeoKianGuan <kianguan@moneyowl.com.sg>
2022-12-30 16:59:01 +0800

    Code clean up not supported page

--- src/app/not-supported/not-supported.component.spec.ts
+++ src/app/not-supported/not-supported.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NotSupportedComponent } from './not-supported.component';
 
@@ -6,7 +6,7 @@ describe('NotSupportedComponent', () => {
   let component: NotSupportedComponent;
   let fixture: ComponentFixture<NotSupportedComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NotSupportedComponent ]
     })
