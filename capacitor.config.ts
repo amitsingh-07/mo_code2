b3fb4d0115d621e8c7ae9454d303599328643d03
NeoKianGuan <kianguan@moneyowl.com.sg>
2023-01-03 10:01:50 +0800

    Commit capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.moneyowl.app',
+  appName: 'MoneyOwl Mobile',
+  webDir: 'dist/NTUCDev',
+  bundledWebRuntime: false
+};
+
+export default config;
