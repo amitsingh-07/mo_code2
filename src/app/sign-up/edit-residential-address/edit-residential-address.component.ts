792efdfaad662d106659f7f2d1b5f105bce4f410
Jasmine Tan <jasmine@moneyowl.com.sg>
2023-01-10 14:42:03 +0800

    fix duplicate lines due to merge

--- src/app/sign-up/edit-residential-address/edit-residential-address.component.ts
+++ src/app/sign-up/edit-residential-address/edit-residential-address.component.ts
@@ -1,7 +1,6 @@
 
 
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
